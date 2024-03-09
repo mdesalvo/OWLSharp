5718180b7f80b51108198ac708fa94be1d4b86dc
mdesalvo <floating_point_9@msn.com>
2024-03-09 11:24:14 +0100

    Move code for bugfix 26 to the most proper validator rule

--- OWLSharp/Validator/Rules/OWLClassTypeRule.cs
+++ OWLSharp/Validator/Rules/OWLClassTypeRule.cs
@@ -12,9 +12,7 @@
 */
 
 using RDFSharp.Model;
-using RDFSharp.Query;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 
 namespace OWLSharp
@@ -32,9 +30,7 @@ namespace OWLSharp
             Dictionary<long, HashSet<long>> disjointWithCache = new Dictionary<long, HashSet<long>>();
             RDFGraph classType = ontology.Data.ABoxGraph[null, RDFVocabulary.RDF.TYPE, null, null];
             RDFGraph complementOf = ontology.Model.ClassModel.TBoxGraph[null, RDFVocabulary.OWL.COMPLEMENT_OF, null, null];
-            RDFGraph aboxObjectAssertions = OWLOntologyDataLoader.GetObjectAssertions(ontology, ontology.Data.ABoxGraph);
-            RDFGraph aboxDataAssertions = OWLOntologyDataLoader.GetDatatypeAssertions(ontology, ontology.Data.ABoxGraph);
-
+            
             //Iterate individuals of the ontology
             IEnumerator<RDFResource> individualsEnumerator = ontology.Data.IndividualsEnumerator;
             while (individualsEnumerator.MoveNext())
@@ -45,7 +41,7 @@ namespace OWLSharp
                                                         .OfType<RDFResource>()
                                                         .ToList();
 
-                //Iterate discovered classes to check for eventual 'owl:disjointWith' or 'owl:complementOf' or local cardinality clashes
+                //Iterate discovered classes to check for eventual 'owl:disjointWith' or 'owl:complementOf' clashes
                 foreach (RDFResource individualClass in individualClasses)
                 {
                     #region Complement Analysis
@@ -75,138 +71,10 @@ namespace OWLSharp
                             $"Violation of 'rdf:type' relations on individual '{individualsEnumerator.Current}'",
                             "Revise your class model: you have disjoint classes to which this individual belongs at the same time!"));
                     #endregion
-
-                    #region Local Cardinality Analysis
-                    //There should not be [Q][Exact|Max|MinMax] local cardinality constraints violated by the individual
-                    bool proceedWithValidation = default, isQualifiedRestriction = default;
-                    RDFResource onProperty = default, onClass = default; 
-                    RDFTypedLiteral maxAllowedCardinality = default;
-                    if (ontology.Model.ClassModel.CheckHasCardinalityRestrictionClass(individualClass))
-                    {
-                        proceedWithValidation = true;
-                        maxAllowedCardinality = OWLOntologyClassModelLoader.GetRestrictionCardinality(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                        onProperty = OWLOntologyClassModelLoader.GetRestrictionProperty(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                    }
-                    else if (ontology.Model.ClassModel.CheckHasMaxCardinalityRestrictionClass(individualClass)
-                              || ontology.Model.ClassModel.CheckHasMinMaxCardinalityRestrictionClass(individualClass))
-                    {
-                        proceedWithValidation = true;
-                        maxAllowedCardinality = OWLOntologyClassModelLoader.GetRestrictionMaxCardinality(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                        onProperty = OWLOntologyClassModelLoader.GetRestrictionProperty(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                    }                        
-                    else if (ontology.Model.ClassModel.CheckHasQualifiedCardinalityRestrictionClass(individualClass))
-                    {
-                        proceedWithValidation = true;
-                        isQualifiedRestriction = true;
-                        maxAllowedCardinality = OWLOntologyClassModelLoader.GetRestrictionQualifiedCardinality(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                        onProperty = OWLOntologyClassModelLoader.GetRestrictionProperty(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                        onClass = OWLOntologyClassModelLoader.GetRestrictionClass(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                    }
-                    else if (ontology.Model.ClassModel.CheckHasMaxQualifiedCardinalityRestrictionClass(individualClass) 
-                              ||ontology.Model.ClassModel.CheckHasMinMaxQualifiedCardinalityRestrictionClass(individualClass))
-                    {
-                        proceedWithValidation = true;
-                        isQualifiedRestriction = true;
-                        maxAllowedCardinality = OWLOntologyClassModelLoader.GetRestrictionMaxQualifiedCardinality(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                        onProperty = OWLOntologyClassModelLoader.GetRestrictionProperty(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                        onClass = OWLOntologyClassModelLoader.GetRestrictionClass(ontology.Model.ClassModel.TBoxGraph, individualClass);
-                    }
-
-                    //Proceed with validation of the current individual
-                    if (proceedWithValidation
-                         && onProperty != null
-                         && uint.TryParse(maxAllowedCardinality.Value, NumberStyles.Integer, CultureInfo.InvariantCulture, out uint maxAllowedCardinalityValue)
-                         && (!isQualifiedRestriction || onClass != null))
-                    {
-                        //Fetch assertions of the current individual using the restricted property
-                        RDFGraph individualAssertions = aboxObjectAssertions[individualsEnumerator.Current, onProperty, null, null]
-                                                         .UnionWith(aboxDataAssertions[individualsEnumerator.Current, onProperty, null, null]);
-                        
-                        //Determine if maximum allowed cardinality is violated or not
-                        if (!isQualifiedRestriction)
-                        {
-                            bool violatesRestriction = default;
-
-                            //Isolate target individuals in order to perform owl:differentFrom analysis (since
-                            //under OWA we must take in consideration only *explicitly different* individuals)
-                            if (ontology.Model.PropertyModel.CheckHasObjectProperty(onProperty))
-                            {
-                                List<RDFResource> targetIndividuals = RDFQueryUtilities.RemoveDuplicates(individualAssertions.Select(t => t.Object)
-                                                                                                                             .OfType<RDFResource>()
-                                                                                                                             .ToList());
-                                violatesRestriction = CountDifferentIndividuals(ontology, targetIndividuals, maxAllowedCardinalityValue) >= maxAllowedCardinalityValue;
-                            }
-
-                            //Isolate target literals in order to perform inequality analysis
-                            else if (ontology.Model.PropertyModel.CheckHasDatatypeProperty(onProperty))
-                            {
-                                List<RDFLiteral> targetLiterals = RDFQueryUtilities.RemoveDuplicates(individualAssertions.Select(t => t.Object)
-                                                                                                                         .OfType<RDFLiteral>()
-                                                                                                                         .ToList());
-                                violatesRestriction = targetLiterals.Count > maxAllowedCardinalityValue;
-                            }
-
-                            //Raise violation error
-                            if (violatesRestriction)
-                                validatorRuleReport.AddEvidence(new OWLValidatorEvidence(
-                                    OWLEnums.OWLValidatorEvidenceCategory.Error,
-                                    nameof(OWLClassTypeRule),
-                                    $"Violation of local cardinality constraint on individual '{individualsEnumerator.Current}'",
-                                    $"Revise your data: you have a local cardinality constraint (valued {maxAllowedCardinalityValue}) violated by the targets of assertion property {onProperty}"));
-                        }                            
-
-                        //Determine if maximum allowed qualified cardinality is violated or not
-                        //(we cannot verify qualified cardinalities working on data properties)
-                        else
-                        {
-                            bool violatesQRestriction = default;
-
-                            //Isolate target individuals in order to perform owl:differentFrom analysis (since
-                            //under OWA we must take in consideration only *explicitly different* individuals)
-                            if (ontology.Model.PropertyModel.CheckHasObjectProperty(onProperty))
-                            {
-                                List<RDFResource> targetQIndividuals = RDFQueryUtilities.RemoveDuplicates(individualAssertions.Where(t => t.TripleFlavor == RDFModelEnums.RDFTripleFlavors.SPO
-                                                                                                                                            && ontology.Data.CheckIsIndividualOf(ontology.Model, (RDFResource)t.Object, onClass))
-                                                                                                                              .Select(t => t.Object)
-                                                                                                                              .OfType<RDFResource>()
-                                                                                                                              .ToList());
-                                violatesQRestriction = CountDifferentIndividuals(ontology, targetQIndividuals, maxAllowedCardinalityValue) >= maxAllowedCardinalityValue;
-                            }
-                            
-                            if (violatesQRestriction)
-                                validatorRuleReport.AddEvidence(new OWLValidatorEvidence(
-                                    OWLEnums.OWLValidatorEvidenceCategory.Error,
-                                    nameof(OWLClassTypeRule),
-                                    $"Violation of local qualified cardinality constraint on individual '{individualsEnumerator.Current}'",
-                                    $"Revise your data: you have a local qualified cardinality constraint (valued {maxAllowedCardinalityValue}) violated by the targets of assertion property {onProperty}"));
-                        }
-                    }
-                    #endregion
                 }                
             }
 
             return validatorRuleReport;
-        }
-
-        //Counts the distinct pairs of individuals related by owl:differentFrom in the given ontology 
-        private static int CountDifferentIndividuals(OWLOntology ontology, List<RDFResource> targetIndividuals, uint maxAllowedCardinalityValue)
-        {
-            //In case the maximum allowed number of occurrences is zero we can safely exit 
-            //by directly reporting the number of target individuals (we are not interested
-            //in computing their owl:differentFrom relations)
-            if (maxAllowedCardinalityValue == 0)
-                return targetIndividuals.Count;
-
-            //Otherwise we must compute the distinct pairs of individuals related by an
-            //owl:differentFrom relation. In fact, under OWA this is the only violation
-            //trigger: having N>threshold explicitly different individuals as targets of
-            //the same [Q][Exact|Max|MinMax] local cardinality constraint 
-            int differentTargetIndividuals = 0;
-            for (int i = 0; i < targetIndividuals.Count - 1; i++)
-                for (int j = i + 1; j < targetIndividuals.Count; j++)
-                    if (ontology.Data.CheckIsDifferentIndividual(targetIndividuals[i], targetIndividuals[j]))
-                        differentTargetIndividuals++;
-            return differentTargetIndividuals;
         }
     }
 }
--- OWLSharp/Validator/Rules/OWLLocalCardinalityRule.cs
+++ OWLSharp/Validator/Rules/OWLLocalCardinalityRule.cs
@@ -12,7 +12,9 @@
 */
 
 using RDFSharp.Model;
+using RDFSharp.Query;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace OWLSharp
@@ -25,18 +27,22 @@ namespace OWLSharp
         internal static OWLValidatorReport ExecuteRule(OWLOntology ontology)
         {
             OWLValidatorReport validatorRuleReport = new OWLValidatorReport();
+            RDFGraph classType = ontology.Data.ABoxGraph[null, RDFVocabulary.RDF.TYPE, null, null];
+            RDFGraph aboxObjectAssertions = OWLOntologyDataLoader.GetObjectAssertions(ontology, ontology.Data.ABoxGraph);
+            RDFGraph aboxDataAssertions = OWLOntologyDataLoader.GetDatatypeAssertions(ontology, ontology.Data.ABoxGraph);
 
             //owl:Restriction
             IEnumerator<RDFResource> restrictionsEnumerator = ontology.Model.ClassModel.RestrictionsEnumerator;
             while (restrictionsEnumerator.MoveNext())
             {
-                //There should not be cardinality restrictions working on a transitive property, or any super properties or inverse properties being transitive
                 if (ontology.Model.ClassModel.CheckHasLocalCardinalityRestrictionClass(restrictionsEnumerator.Current)
                      || ontology.Model.ClassModel.CheckHasLocalQualifiedCardinalityRestrictionClass(restrictionsEnumerator.Current))
                 {
-                    //Grab owl:onProperty value to check if it is a transitive property
                     RDFResource onProperty = ontology.Model.ClassModel.TBoxGraph[restrictionsEnumerator.Current, RDFVocabulary.OWL.ON_PROPERTY, null, null]
                                                 .FirstOrDefault()?.Object as RDFResource;
+
+                    #region Transitivity Analysis
+                    //There should not be cardinality restrictions working on a transitive property, or any super properties or inverse properties being transitive
                     if (ontology.Model.PropertyModel.CheckHasTransitiveProperty(onProperty))
                         validatorRuleReport.AddEvidence(new OWLValidatorEvidence(
                             OWLEnums.OWLValidatorEvidenceCategory.Error,
@@ -45,7 +51,6 @@ namespace OWLSharp
                             "Revise your class model: it is not allowed the direct use of transitive properties on cardinality restrictions"));
                     else
                     {
-                        //Grab its super properties to check for presence of any transitive properties
                         List<RDFResource> superProperties = ontology.Model.PropertyModel.GetSuperPropertiesOf(onProperty);
                         if (superProperties.Any(sp => ontology.Model.PropertyModel.CheckHasTransitiveProperty(sp)))
                             validatorRuleReport.AddEvidence(new OWLValidatorEvidence(
@@ -63,10 +68,144 @@ namespace OWLSharp
                                 $"Violation of OWL-DL integrity caused by 'owl:TransitiveProperty' being inverse property of 'owl:onProperty' of cardinality restriction '{restrictionsEnumerator.Current}'",
                                 "Revise your class model: it is not allowed the indirect use of transitive properties on cardinality restrictions"));
                     }
+                    #endregion
+
+                    #region Local Cardinality Analysis
+                    //There should not be [Q][Exact|Max|MinMax] local cardinality constraints violated by the individual
+                    bool proceedWithValidation = default, isQualifiedRestriction = default;
+                    RDFResource onClass = default; 
+                    RDFTypedLiteral maxAllowedCardinality = default;
+                    if (ontology.Model.ClassModel.CheckHasCardinalityRestrictionClass(restrictionsEnumerator.Current))
+                    {
+                        proceedWithValidation = true;
+                        maxAllowedCardinality = OWLOntologyClassModelLoader.GetRestrictionCardinality(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                        onProperty = OWLOntologyClassModelLoader.GetRestrictionProperty(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                    }
+                    else if (ontology.Model.ClassModel.CheckHasMaxCardinalityRestrictionClass(restrictionsEnumerator.Current)
+                              || ontology.Model.ClassModel.CheckHasMinMaxCardinalityRestrictionClass(restrictionsEnumerator.Current))
+                    {
+                        proceedWithValidation = true;
+                        maxAllowedCardinality = OWLOntologyClassModelLoader.GetRestrictionMaxCardinality(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                        onProperty = OWLOntologyClassModelLoader.GetRestrictionProperty(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                    }                        
+                    else if (ontology.Model.ClassModel.CheckHasQualifiedCardinalityRestrictionClass(restrictionsEnumerator.Current))
+                    {
+                        proceedWithValidation = true;
+                        isQualifiedRestriction = true;
+                        maxAllowedCardinality = OWLOntologyClassModelLoader.GetRestrictionQualifiedCardinality(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                        onProperty = OWLOntologyClassModelLoader.GetRestrictionProperty(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                        onClass = OWLOntologyClassModelLoader.GetRestrictionClass(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                    }
+                    else if (ontology.Model.ClassModel.CheckHasMaxQualifiedCardinalityRestrictionClass(restrictionsEnumerator.Current) 
+                              ||ontology.Model.ClassModel.CheckHasMinMaxQualifiedCardinalityRestrictionClass(restrictionsEnumerator.Current))
+                    {
+                        proceedWithValidation = true;
+                        isQualifiedRestriction = true;
+                        maxAllowedCardinality = OWLOntologyClassModelLoader.GetRestrictionMaxQualifiedCardinality(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                        onProperty = OWLOntologyClassModelLoader.GetRestrictionProperty(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                        onClass = OWLOntologyClassModelLoader.GetRestrictionClass(ontology.Model.ClassModel.TBoxGraph, restrictionsEnumerator.Current);
+                    }
+
+                    if (proceedWithValidation
+                        && onProperty != null
+                        && uint.TryParse(maxAllowedCardinality.Value, NumberStyles.Integer, CultureInfo.InvariantCulture, out uint maxAllowedCardinalityValue)
+                        && (!isQualifiedRestriction || onClass != null))
+                    {
+                        //Fetch individuals explicitly assigned to the current local cardinality constraints
+                        foreach(RDFResource restrictionIndividual in  classType[null, null, restrictionsEnumerator.Current, null]
+                                                                        .Select(t => t.Subject)
+                                                                        .OfType<RDFResource>())
+                        {
+                            //Fetch assertions of the current individual on the restricted property
+                            RDFGraph individualAssertions = aboxObjectAssertions[restrictionIndividual, onProperty, null, null]
+                                                             .UnionWith(aboxDataAssertions[restrictionIndividual, onProperty, null, null]);
+                            
+                            //Determine if maximum allowed cardinality is violated or not
+                            if (!isQualifiedRestriction)
+                            {
+                                bool violatesRestriction = default;
+
+                                //Isolate target individuals in order to perform owl:differentFrom analysis (since
+                                //under OWA we must take in consideration only *explicitly different* individuals)
+                                if (ontology.Model.PropertyModel.CheckHasObjectProperty(onProperty))
+                                {
+                                    List<RDFResource> targetIndividuals = RDFQueryUtilities.RemoveDuplicates(individualAssertions.Select(t => t.Object)
+                                                                                                                                 .OfType<RDFResource>()
+                                                                                                                                 .ToList());
+                                    violatesRestriction = CountDifferentIndividuals(ontology, targetIndividuals, maxAllowedCardinalityValue) >= maxAllowedCardinalityValue;
+                                }
+
+                                //Isolate target literals in order to perform inequality analysis
+                                else if (ontology.Model.PropertyModel.CheckHasDatatypeProperty(onProperty))
+                                {
+                                    List<RDFLiteral> targetLiterals = RDFQueryUtilities.RemoveDuplicates(individualAssertions.Select(t => t.Object)
+                                                                                                                             .OfType<RDFLiteral>()
+                                                                                                                             .ToList());
+                                    violatesRestriction = targetLiterals.Count > maxAllowedCardinalityValue;
+                                }
+
+                                //Raise violation error
+                                if (violatesRestriction)
+                                    validatorRuleReport.AddEvidence(new OWLValidatorEvidence(
+                                        OWLEnums.OWLValidatorEvidenceCategory.Error,
+                                        nameof(OWLClassTypeRule),
+                                        $"Violation of local cardinality constraint on individual '{restrictionIndividual}'",
+                                        $"Revise your data: you have a local cardinality constraint (valued {maxAllowedCardinalityValue}) violated by the targets of assertion property {onProperty}"));
+                            }                            
+
+                            //Determine if maximum allowed qualified cardinality is violated or not
+                            //(we cannot verify qualified cardinalities working on data properties)
+                            else
+                            {
+                                bool violatesQRestriction = default;
+
+                                //Isolate target individuals in order to perform owl:differentFrom analysis (since
+                                //under OWA we must take in consideration only *explicitly different* individuals)
+                                if (ontology.Model.PropertyModel.CheckHasObjectProperty(onProperty))
+                                {
+                                    List<RDFResource> targetQIndividuals = RDFQueryUtilities.RemoveDuplicates(individualAssertions.Where(t => t.TripleFlavor == RDFModelEnums.RDFTripleFlavors.SPO
+                                                                                                                                                && ontology.Data.CheckIsIndividualOf(ontology.Model, (RDFResource)t.Object, onClass))
+                                                                                                                                .Select(t => t.Object)
+                                                                                                                                .OfType<RDFResource>()
+                                                                                                                                .ToList());
+                                    violatesQRestriction = CountDifferentIndividuals(ontology, targetQIndividuals, maxAllowedCardinalityValue) >= maxAllowedCardinalityValue;
+                                }
+                                
+                                if (violatesQRestriction)
+                                    validatorRuleReport.AddEvidence(new OWLValidatorEvidence(
+                                        OWLEnums.OWLValidatorEvidenceCategory.Error,
+                                        nameof(OWLClassTypeRule),
+                                        $"Violation of local qualified cardinality constraint on individual '{restrictionIndividual}'",
+                                        $"Revise your data: you have a local qualified cardinality constraint (valued {maxAllowedCardinalityValue}) violated by the targets of assertion property {onProperty}"));
+                            }
+                        }
+                    }
+                    #endregion
                 }   
             }
 
             return validatorRuleReport;
+        }
+
+        //Counts the distinct pairs of individuals related by owl:differentFrom in the given ontology 
+        private static int CountDifferentIndividuals(OWLOntology ontology, List<RDFResource> targetIndividuals, uint maxAllowedCardinalityValue)
+        {
+            //In case the maximum allowed number of occurrences is zero we can safely exit 
+            //by directly reporting the number of target individuals (we are not interested
+            //in computing their owl:differentFrom relations)
+            if (maxAllowedCardinalityValue == 0)
+                return targetIndividuals.Count;
+
+            //Otherwise we must compute the distinct pairs of individuals related by an
+            //owl:differentFrom relation. In fact, under OWA this is the only violation
+            //trigger: having N>threshold explicitly different individuals as targets of
+            //the same [Q][Exact|Max|MinMax] local cardinality constraint 
+            int differentTargetIndividuals = 0;
+            for (int i = 0; i < targetIndividuals.Count - 1; i++)
+                for (int j = i + 1; j < targetIndividuals.Count; j++)
+                    if (ontology.Data.CheckIsDifferentIndividual(targetIndividuals[i], targetIndividuals[j]))
+                        differentTargetIndividuals++;
+            return differentTargetIndividuals;
         }
     }
 }
