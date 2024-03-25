84655b18b94236ef773a44736d1af63cc7c31dfd
mdesalvo <floating_point_9@msn.com>
2024-03-25 19:35:56 +0100

    .

--- OWLSharp.Test/Serialization/OWLXMLSerializerTest.cs
+++ OWLSharp.Test/Serialization/OWLXMLSerializerTest.cs
@@ -279,6 +279,40 @@ namespace OWLSharp.Test.Serialization
             Assert.IsTrue(ontology2.Imports.Count == 1);
             Assert.IsTrue(ontology2.Axioms.Count == 1);
         }
+
+        [TestMethod]
+        public void ShouldSerializeAndDeserializeOntologyWithDisjointClasssAxiomHavingObjectUnionOfAxiomWithNestedObjectIntersectionOfAxiom()
+        {
+            OWLOntology ontology = new OWLOntology(new Uri("http://example.org/"), new Uri("http://example.org/v1"));
+            ontology.Prefixes.Add(new OWLPrefix(RDFNamespaceRegister.GetByPrefix(RDFVocabulary.FOAF.PREFIX)));
+            ontology.Imports.Add(new OWLImport(new RDFResource("http://example.org/import/")));
+            ontology.Axioms.Add(new OWLDisjointClassesAxiom(new List<OWLClassExpression>()
+            {
+                new OWLClass(new RDFResource("http://example.org/Cls1")),
+                new OWLObjectUnionOf(new List<OWLClassExpression>()
+                {
+                    new OWLClass(new RDFResource("http://example.org/Cls2")),
+                    new OWLClass(new RDFResource("http://example.org/Cls3")),
+                    new OWLObjectIntersectionOf(new List<OWLClassExpression>()
+                    {
+                        new OWLClass(new XmlQualifiedName("Agent", RDFVocabulary.FOAF.BASE_URI)),
+                        new OWLClass(new XmlQualifiedName("Person", RDFVocabulary.FOAF.BASE_URI)),
+                        new OWLClass(new XmlQualifiedName("Organization", RDFVocabulary.FOAF.BASE_URI)),
+                    })
+                })
+            }));
+
+            string owxOntology = OWLSerializer.Serialize(ontology);
+
+            OWLOntology ontology2 = OWLSerializer.Deserialize(owxOntology);
+
+            Assert.IsNotNull(ontology2);
+            Assert.IsTrue(string.Equals(ontology2.OntologyIRI, "http://example.org/"));
+            Assert.IsTrue(string.Equals(ontology2.OntologyVersion, "http://example.org/v1"));
+            Assert.IsTrue(ontology2.Prefixes.Count == 6);
+            Assert.IsTrue(ontology2.Imports.Count == 1);
+            Assert.IsTrue(ontology2.Axioms.Count == 1);
+        }
         #endregion
     }
 }
