cc86962cc49430f3e2483067b113dc08bec63f1a
mdesalvo <floating_point_9@msn.com>
2024-03-26 20:18:16 +0100

    .

--- OWLSharp/Serialization/OWLOntology.cs
+++ OWLSharp/Serialization/OWLOntology.cs
@@ -42,7 +42,7 @@ namespace OWLSharp
         [XmlElement(typeof(OWLSubClassOfAxiom), ElementName="SubClassOf")]
         [XmlElement(typeof(OWLEquivalentClassesAxiom), ElementName="EquivalentClasses")]
         [XmlElement(typeof(OWLDisjointClassesAxiom), ElementName="DisjointClasses")]
-        [XmlElement(typeof(OWLDisjointUnionAxiom), ElementName = "DisjointUnion")]
+        [XmlElement(typeof(OWLDisjointUnionAxiom), ElementName="DisjointUnion")]
         public List<OWLAxiom> Axioms { get; set; }
         #endregion
 
