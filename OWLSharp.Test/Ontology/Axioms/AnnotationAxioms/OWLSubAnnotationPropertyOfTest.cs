c8857dea6c04a42e8b5c89592cdcb7858e2aee35
Marco De Salvo <marco.desalvo@vodafone.com>
2024-04-12 10:49:24 +0200

    .

--- OWLSharp.Test/Ontology/Axioms/AnnotationAxioms/OWLSubAnnotationPropertyOfTest.cs
+++ OWLSharp.Test/Ontology/Axioms/AnnotationAxioms/OWLSubAnnotationPropertyOfTest.cs
@@ -133,6 +133,107 @@ namespace OWLSharp.Ontology.Axioms.Test
                             && string.Equals(annPropDom.SubAnnotationProperty.IRI, RDFVocabulary.DC.DCTERMS.TITLE.ToString())
                             && string.Equals(annPropDom.SuperAnnotationProperty.IRI, RDFVocabulary.DC.TITLE.ToString()));
         }
+
+        [TestMethod]
+        public void ShouldSerializeMultipleAndNestedSubAnnotationPropertyOfViaOntology()
+        {
+            OWLOntology ontology = new OWLOntology();
+            ontology.Prefixes.Add(new OWLPrefix(RDFNamespaceRegister.GetByPrefix(RDFVocabulary.FOAF.PREFIX)));
+            ontology.AnnotationAxioms.Add(
+                new OWLSubAnnotationPropertyOf(
+                    new OWLAnnotationProperty(RDFVocabulary.DC.DCTERMS.TITLE),
+                    new OWLAnnotationProperty(RDFVocabulary.DC.TITLE))
+                {
+                    Annotations = new List<OWLAnnotation>()
+                    {
+                        new OWLAnnotation(
+                            new OWLAnnotationProperty(RDFVocabulary.DC.DESCRIPTION),
+                            new RDFResource("ex:AnnValue"))
+                        {
+                            Annotation = new OWLAnnotation(
+                                new OWLAnnotationProperty(RDFVocabulary.DC.CONTRIBUTOR),
+                                new OWLLiteral(new RDFPlainLiteral("contributor", "en-us--rtl")))
+                        }
+                    }
+                });
+            ontology.AnnotationAxioms.Add(
+                new OWLSubAnnotationPropertyOf(
+                    new OWLAnnotationProperty(RDFVocabulary.DC.DCTERMS.CREATOR),
+                    new OWLAnnotationProperty(RDFVocabulary.DC.CREATOR)));
+            string serializedXML = OWLSerializer.Serialize(ontology);
+
+            Assert.IsTrue(string.Equals(serializedXML,
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Ontology xmlns:owl=""http://www.w3.org/2002/07/owl#"" xmlns:rdfs=""http://www.w3.org/2000/01/rdf-schema#"" xmlns:rdf=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema#"" xmlns:foaf=""http://xmlns.com/foaf/0.1/"">
+  <Prefix name=""owl"" IRI=""http://www.w3.org/2002/07/owl#"" />
+  <Prefix name=""rdfs"" IRI=""http://www.w3.org/2000/01/rdf-schema#"" />
+  <Prefix name=""rdf"" IRI=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" />
+  <Prefix name=""xsd"" IRI=""http://www.w3.org/2001/XMLSchema#"" />
+  <Prefix name=""xml"" IRI=""http://www.w3.org/XML/1998/namespace"" />
+  <Prefix name=""foaf"" IRI=""http://xmlns.com/foaf/0.1/"" />
+  <SubAnnotationPropertyOf>
+    <Annotation>
+      <Annotation>
+        <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/contributor"" />
+        <Literal xml:lang=""EN-US--RTL"">contributor</Literal>
+      </Annotation>
+      <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/description"" />
+      <IRI>ex:AnnValue</IRI>
+    </Annotation>
+    <AnnotationProperty IRI=""http://purl.org/dc/terms/title"" />
+    <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/title"" />
+  </SubAnnotationPropertyOf>
+  <SubAnnotationPropertyOf>
+    <AnnotationProperty IRI=""http://purl.org/dc/terms/creator"" />
+    <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/creator"" />
+  </SubAnnotationPropertyOf>
+</Ontology>"));
+        }
+
+        [TestMethod]
+        public void ShouldDeserializeMultipleAndNestedAnnotationAssertionsViaOntology()
+        {
+            OWLOntology ontology = OWLSerializer.Deserialize(
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Ontology xmlns:owl=""http://www.w3.org/2002/07/owl#"" xmlns:rdfs=""http://www.w3.org/2000/01/rdf-schema#"" xmlns:rdf=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema#"" xmlns:foaf=""http://xmlns.com/foaf/0.1/"">
+  <Prefix name=""owl"" IRI=""http://www.w3.org/2002/07/owl#"" />
+  <Prefix name=""rdfs"" IRI=""http://www.w3.org/2000/01/rdf-schema#"" />
+  <Prefix name=""rdf"" IRI=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" />
+  <Prefix name=""xsd"" IRI=""http://www.w3.org/2001/XMLSchema#"" />
+  <Prefix name=""xml"" IRI=""http://www.w3.org/XML/1998/namespace"" />
+  <Prefix name=""foaf"" IRI=""http://xmlns.com/foaf/0.1/"" />
+  <SubAnnotationPropertyOf>
+    <Annotation>
+      <Annotation>
+        <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/contributor"" />
+        <Literal xml:lang=""EN-US--RTL"">contributor</Literal>
+      </Annotation>
+      <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/description"" />
+      <IRI>ex:AnnValue</IRI>
+    </Annotation>
+    <AnnotationProperty IRI=""http://purl.org/dc/terms/title"" />
+    <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/title"" />
+  </SubAnnotationPropertyOf>
+  <SubAnnotationPropertyOf>
+    <AnnotationProperty IRI=""http://purl.org/dc/terms/creator"" />
+    <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/creator"" />
+  </SubAnnotationPropertyOf>
+</Ontology>");
+
+            Assert.IsNotNull(ontology);
+            Assert.IsTrue(ontology.AnnotationAxioms.Count == 2);
+            Assert.IsTrue(ontology.AnnotationAxioms.Any(annAxm => annAxm is OWLSubAnnotationPropertyOf subannOf
+                            && string.Equals(subannOf.SubAnnotationProperty.IRI.ToString(), "http://purl.org/dc/terms/title")
+                            && string.Equals(subannOf.SuperAnnotationProperty.IRI.ToLower(), "http://purl.org/dc/elements/1.1/title")
+                                && string.Equals(subannOf.Annotations.Single().AnnotationProperty.IRI, "http://purl.org/dc/elements/1.1/description")
+                                && string.Equals(subannOf.Annotations.Single().ValueIRI, "ex:AnnValue")
+                                    && string.Equals(subannOf.Annotations.Single().Annotation.AnnotationProperty.IRI, "http://purl.org/dc/elements/1.1/contributor")
+                                    && string.Equals(subannOf.Annotations.Single().Annotation.ValueLiteral.Value, "contributor")
+                                    && string.Equals(subannOf.Annotations.Single().Annotation.ValueLiteral.Language, "EN-US--RTL")));
+            Assert.IsTrue(ontology.AnnotationAxioms.Any(annAxm => annAxm is OWLSubAnnotationPropertyOf subannProp
+                            && string.Equals(subannProp.SubAnnotationProperty.IRI.ToString(), "http://purl.org/dc/terms/creator")
+                            && string.Equals(subannProp.SuperAnnotationProperty.IRI, "http://purl.org/dc/elements/1.1/creator")));
+        }
         #endregion
     }
 }
