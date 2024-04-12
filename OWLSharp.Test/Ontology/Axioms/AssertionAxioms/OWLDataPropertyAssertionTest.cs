d8ce281bcb8b1386e83be9a00f34b9cdad23f68e
mdesalvo <floating_point_9@msn.com>
2024-04-12 20:03:52 +0200

    .

--- OWLSharp.Test/Ontology/Axioms/AssertionAxioms/OWLDataPropertyAssertionTest.cs
+++ OWLSharp.Test/Ontology/Axioms/AssertionAxioms/OWLDataPropertyAssertionTest.cs
@@ -18,6 +18,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OWLSharp.Ontology.Expressions;
 using OWLSharp.Test;
 using RDFSharp.Model;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace OWLSharp.Ontology.Axioms.Test
@@ -79,6 +80,24 @@ namespace OWLSharp.Ontology.Axioms.Test
         }
 
 		[TestMethod]
+        public void ShouldSerializeNamedIndividualDataPropertyAssertionViaOntology()
+        {
+			OWLOntology ontology = new OWLOntology();
+			ontology.AssertionAxioms.Add(
+				new OWLDataPropertyAssertion(
+					new OWLDataProperty(RDFVocabulary.FOAF.AGE),
+					new OWLNamedIndividual(new RDFResource("ex:Bob")),
+					new OWLLiteral(new RDFTypedLiteral("25", RDFModelEnums.RDFDatatypes.XSD_INTEGER)))
+					{
+						Annotations = [ new OWLAnnotation(new OWLAnnotationProperty(RDFVocabulary.DC.CONTRIBUTOR), new OWLLiteral(new RDFPlainLiteral("Steve","en"))) ]
+					});
+            string serializedXML = OWLTestSerializer<OWLOntology>.Serialize(ontology);
+
+            Assert.IsTrue(string.Equals(serializedXML,
+@"<Ontology><Prefix name=""owl"" IRI=""http://www.w3.org/2002/07/owl#"" /><Prefix name=""rdfs"" IRI=""http://www.w3.org/2000/01/rdf-schema#"" /><Prefix name=""rdf"" IRI=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" /><Prefix name=""xsd"" IRI=""http://www.w3.org/2001/XMLSchema#"" /><Prefix name=""xml"" IRI=""http://www.w3.org/XML/1998/namespace"" /><DataPropertyAssertion><Annotation><AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/contributor"" /><Literal xml:lang=""EN"">Steve</Literal></Annotation><DataProperty IRI=""http://xmlns.com/foaf/0.1/age"" /><NamedIndividual IRI=""ex:Bob"" /><Literal datatypeIRI=""http://www.w3.org/2001/XMLSchema#integer"">25</Literal></DataPropertyAssertion></Ontology>"));
+        }
+
+		[TestMethod]
         public void ShouldDeserializeNamedIndividualDataPropertyAssertion()
         {
             OWLDataPropertyAssertion dataPropertyAssertion = OWLTestSerializer<OWLDataPropertyAssertion>.Deserialize(
@@ -93,6 +112,41 @@ namespace OWLSharp.Ontology.Axioms.Test
             Assert.IsTrue(string.Equals(dataPropertyAssertion.Literal.Value, "25"));
 			Assert.IsTrue(string.Equals(dataPropertyAssertion.Literal.DatatypeIRI, "http://www.w3.org/2001/XMLSchema#integer"));
 		}
+
+		[TestMethod]
+        public void ShouldDeserializeNamedIndividualDataPropertyAssertionViaOntology()
+        {
+			OWLOntology ontology = OWLSerializer.Deserialize(
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Ontology xmlns:owl=""http://www.w3.org/2002/07/owl#"" xmlns:rdfs=""http://www.w3.org/2000/01/rdf-schema#"" xmlns:rdf=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema#"">
+    <Prefix name=""owl"" IRI=""http://www.w3.org/2002/07/owl#"" />
+    <Prefix name=""rdfs"" IRI=""http://www.w3.org/2000/01/rdf-schema#"" />
+    <Prefix name=""rdf"" IRI=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" />
+    <Prefix name=""xsd"" IRI=""http://www.w3.org/2001/XMLSchema#"" />
+    <Prefix name=""xml"" IRI=""http://www.w3.org/XML/1998/namespace"" />
+    <DataPropertyAssertion>
+        <Annotation>
+            <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/contributor"" />
+            <Literal xml:lang=""EN"">Steve</Literal>
+        </Annotation>
+        <DataProperty IRI=""http://xmlns.com/foaf/0.1/age"" />
+        <NamedIndividual IRI=""ex:Bob"" />
+        <Literal datatypeIRI=""http://www.w3.org/2001/XMLSchema#integer"">25</Literal>
+    </DataPropertyAssertion>
+</Ontology>");
+
+			Assert.IsNotNull(ontology);
+            Assert.IsTrue(ontology.AssertionAxioms.Count == 1);
+            Assert.IsTrue(ontology.AssertionAxioms.Single() is OWLDataPropertyAssertion dpAsn
+                            && string.Equals(dpAsn.DataProperty.IRI, "http://xmlns.com/foaf/0.1/age")
+                            && string.Equals(((OWLNamedIndividual)dpAsn.IndividualExpression).IRI, "ex:Bob")
+							&& string.Equals(dpAsn.Literal.Value, "25")
+							&& string.Equals(dpAsn.Literal.DatatypeIRI, "http://www.w3.org/2001/XMLSchema#integer"));
+			Assert.IsTrue(ontology.AssertionAxioms.Single() is OWLDataPropertyAssertion dpAsn1
+							&& string.Equals(dpAsn1.Annotations.Single().AnnotationProperty.IRI, "http://purl.org/dc/elements/1.1/contributor")
+							&& string.Equals(dpAsn1.Annotations.Single().ValueLiteral.Value, "Steve")
+							&& string.Equals(dpAsn1.Annotations.Single().ValueLiteral.Language, "EN"));
+        }
 
 		[TestMethod]
         public void ShouldCreateAnonymousIndividualDataPropertyAssertion()
