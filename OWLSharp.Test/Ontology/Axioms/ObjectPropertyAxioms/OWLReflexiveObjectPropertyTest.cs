0bdb283db2062a61dc60f43956b57fc5258cbebd
Marco De Salvo <marco.desalvo@vodafone.com>
2024-04-17 11:43:11 +0200

    .

---
+++ OWLSharp.Test/Ontology/Axioms/ObjectPropertyAxioms/OWLReflexiveObjectPropertyTest.cs
@@ -0,0 +1,204 @@
+/*
+   Copyright 2014-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OWLSharp.Ontology.Expressions;
+using OWLSharp.Test;
+using RDFSharp.Model;
+
+namespace OWLSharp.Ontology.Axioms.Test
+{
+	[TestClass]
+    public class OWLReflexiveObjectPropertyTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldCreateReflexiveObjectProperty()
+        {
+			OWLReflexiveObjectProperty reflexiveObjectProperty = new OWLReflexiveObjectProperty(
+				new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS));
+
+			Assert.IsNotNull(reflexiveObjectProperty);
+			Assert.IsNotNull(reflexiveObjectProperty.ObjectPropertyExpression);
+            Assert.IsTrue(reflexiveObjectProperty.ObjectPropertyExpression is OWLObjectProperty objProp
+                            && string.Equals(objProp.IRI, RDFVocabulary.FOAF.KNOWS.ToString()));
+		}
+
+        [TestMethod]
+        public void ShouldCreateReflexiveObjectInverseOf()
+        {
+            OWLReflexiveObjectProperty reflexiveObjectProperty = new OWLReflexiveObjectProperty(
+                new OWLObjectInverseOf(new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)));
+
+            Assert.IsNotNull(reflexiveObjectProperty);
+            Assert.IsNotNull(reflexiveObjectProperty.ObjectPropertyExpression);
+            Assert.IsTrue(reflexiveObjectProperty.ObjectPropertyExpression is OWLObjectInverseOf objInvOf
+                            && string.Equals(objInvOf.ObjectProperty.IRI, RDFVocabulary.FOAF.KNOWS.ToString()));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingReflexiveObjectPropertyBecauseNullObjectProperty()
+            => Assert.ThrowsException<OWLException>(() => new OWLReflexiveObjectProperty(null as OWLObjectProperty));
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnCreatingReflexiveObjectPropertyBecauseNullObjectInverseOf()
+            => Assert.ThrowsException<OWLException>(() => new OWLReflexiveObjectProperty(null as OWLObjectInverseOf));
+
+        [TestMethod]
+        public void ShouldSerializeReflexiveObjectProperty()
+        {
+            OWLReflexiveObjectProperty reflexiveObjectProperty = new OWLReflexiveObjectProperty(
+				new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS));
+            string serializedXML = OWLTestSerializer<OWLReflexiveObjectProperty>.Serialize(reflexiveObjectProperty);
+
+            Assert.IsTrue(string.Equals(serializedXML,
+@"<ReflexiveObjectProperty><ObjectProperty IRI=""http://xmlns.com/foaf/0.1/knows"" /></ReflexiveObjectProperty>"));
+        }
+
+        [TestMethod]
+        public void ShouldSerializeReflexiveObjectInverseOf()
+        {
+            OWLReflexiveObjectProperty reflexiveObjectProperty = new OWLReflexiveObjectProperty(
+                new OWLObjectInverseOf(new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)));
+            string serializedXML = OWLTestSerializer<OWLReflexiveObjectProperty>.Serialize(reflexiveObjectProperty);
+
+            Assert.IsTrue(string.Equals(serializedXML,
+@"<ReflexiveObjectProperty><ObjectInverseOf><ObjectProperty IRI=""http://xmlns.com/foaf/0.1/knows"" /></ObjectInverseOf></ReflexiveObjectProperty>"));
+        }
+
+        [TestMethod]
+        public void ShouldSerializeReflexiveObjectPropertyViaOntology()
+        {
+            OWLOntology ontology = new OWLOntology();
+            ontology.ObjectPropertyAxioms.Add(
+                new OWLReflexiveObjectProperty(
+					new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)));
+            string serializedXML = OWLTestSerializer<OWLOntology>.Serialize(ontology);
+
+            Assert.IsTrue(string.Equals(serializedXML,
+@"<Ontology><Prefix name=""owl"" IRI=""http://www.w3.org/2002/07/owl#"" /><Prefix name=""rdfs"" IRI=""http://www.w3.org/2000/01/rdf-schema#"" /><Prefix name=""rdf"" IRI=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" /><Prefix name=""xsd"" IRI=""http://www.w3.org/2001/XMLSchema#"" /><Prefix name=""xml"" IRI=""http://www.w3.org/XML/1998/namespace"" /><ReflexiveObjectProperty><ObjectProperty IRI=""http://xmlns.com/foaf/0.1/knows"" /></ReflexiveObjectProperty></Ontology>"));
+        }
+
+        [TestMethod]
+        public void ShouldSerializeReflexiveObjectInverseOfViaOntology()
+        {
+            OWLOntology ontology = new OWLOntology();
+            ontology.ObjectPropertyAxioms.Add(
+                new OWLReflexiveObjectProperty(
+                    new OWLObjectInverseOf(new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS))));
+            string serializedXML = OWLTestSerializer<OWLOntology>.Serialize(ontology);
+
+            Assert.IsTrue(string.Equals(serializedXML,
+@"<Ontology><Prefix name=""owl"" IRI=""http://www.w3.org/2002/07/owl#"" /><Prefix name=""rdfs"" IRI=""http://www.w3.org/2000/01/rdf-schema#"" /><Prefix name=""rdf"" IRI=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" /><Prefix name=""xsd"" IRI=""http://www.w3.org/2001/XMLSchema#"" /><Prefix name=""xml"" IRI=""http://www.w3.org/XML/1998/namespace"" /><ReflexiveObjectProperty><ObjectInverseOf><ObjectProperty IRI=""http://xmlns.com/foaf/0.1/knows"" /></ObjectInverseOf></ReflexiveObjectProperty></Ontology>"));
+        }
+
+        [TestMethod]
+        public void ShouldDeserializeReflexiveObjectProperty()
+        {
+            OWLReflexiveObjectProperty reflexiveObjectProperty = OWLTestSerializer<OWLReflexiveObjectProperty>.Deserialize(
+@"<ReflexiveObjectProperty>
+  <ObjectProperty IRI=""http://xmlns.com/foaf/0.1/knows"" />
+</ReflexiveObjectProperty>");
+
+            Assert.IsNotNull(reflexiveObjectProperty);
+			Assert.IsNotNull(reflexiveObjectProperty.ObjectPropertyExpression);
+            Assert.IsTrue(reflexiveObjectProperty.ObjectPropertyExpression is OWLObjectProperty objProp
+                            && string.Equals(objProp.IRI, RDFVocabulary.FOAF.KNOWS.ToString()));
+        }
+
+        [TestMethod]
+        public void ShouldDeserializeReflexiveObjectInverseOf()
+        {
+            OWLReflexiveObjectProperty reflexiveObjectProperty = OWLTestSerializer<OWLReflexiveObjectProperty>.Deserialize(
+@"<ReflexiveObjectProperty>
+  <ObjectInverseOf>
+    <ObjectProperty IRI=""http://xmlns.com/foaf/0.1/knows"" />
+  </ObjectInverseOf>
+</ReflexiveObjectProperty>");
+
+            Assert.IsNotNull(reflexiveObjectProperty);
+            Assert.IsNotNull(reflexiveObjectProperty.ObjectPropertyExpression);
+            Assert.IsTrue(reflexiveObjectProperty.ObjectPropertyExpression is OWLObjectInverseOf objInvOf
+                            && string.Equals(objInvOf.ObjectProperty.IRI, RDFVocabulary.FOAF.KNOWS.ToString()));
+        }
+
+        [TestMethod]
+        public void ShouldDeserializeReflexiveObjectPropertyViaOntology()
+        {
+            OWLOntology ontology = OWLSerializer.Deserialize(
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Ontology xmlns:owl=""http://www.w3.org/2002/07/owl#"" xmlns:rdfs=""http://www.w3.org/2000/01/rdf-schema#"" xmlns:rdf=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema#"">
+  <Prefix name=""owl"" IRI=""http://www.w3.org/2002/07/owl#"" />
+  <Prefix name=""rdfs"" IRI=""http://www.w3.org/2000/01/rdf-schema#"" />
+  <Prefix name=""rdf"" IRI=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" />
+  <Prefix name=""xsd"" IRI=""http://www.w3.org/2001/XMLSchema#"" />
+  <Prefix name=""xml"" IRI=""http://www.w3.org/XML/1998/namespace"" />
+  <ReflexiveObjectProperty>
+    <Annotation>
+	  <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/contributor"" />
+	  <Literal xml:lang=""EN"">Steve</Literal>
+	</Annotation>
+    <ObjectProperty IRI=""http://xmlns.com/foaf/0.1/knows"" />
+  </ReflexiveObjectProperty>
+</Ontology>");
+
+            Assert.IsNotNull(ontology);
+            Assert.IsTrue(ontology.ObjectPropertyAxioms.Count == 1);
+            Assert.IsTrue(ontology.ObjectPropertyAxioms.Single() is OWLReflexiveObjectProperty reflObjProp
+                            && reflObjProp.ObjectPropertyExpression is OWLObjectProperty objProp
+                            && string.Equals(objProp.IRI, RDFVocabulary.FOAF.KNOWS.ToString()));
+			Assert.IsTrue(ontology.ObjectPropertyAxioms.Single() is OWLReflexiveObjectProperty reflObjProp1
+							&& string.Equals(reflObjProp1.Annotations.Single().AnnotationProperty.IRI, "http://purl.org/dc/elements/1.1/contributor")
+							&& string.Equals(reflObjProp1.Annotations.Single().ValueLiteral.Value, "Steve")
+							&& string.Equals(reflObjProp1.Annotations.Single().ValueLiteral.Language, "EN"));
+        }
+
+        [TestMethod]
+        public void ShouldDeserializeReflexiveObjectInverseOfViaOntology()
+        {
+            OWLOntology ontology = OWLSerializer.Deserialize(
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Ontology xmlns:owl=""http://www.w3.org/2002/07/owl#"" xmlns:rdfs=""http://www.w3.org/2000/01/rdf-schema#"" xmlns:rdf=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema#"">
+  <Prefix name=""owl"" IRI=""http://www.w3.org/2002/07/owl#"" />
+  <Prefix name=""rdfs"" IRI=""http://www.w3.org/2000/01/rdf-schema#"" />
+  <Prefix name=""rdf"" IRI=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"" />
+  <Prefix name=""xsd"" IRI=""http://www.w3.org/2001/XMLSchema#"" />
+  <Prefix name=""xml"" IRI=""http://www.w3.org/XML/1998/namespace"" />
+  <ReflexiveObjectProperty>
+    <Annotation>
+	  <AnnotationProperty IRI=""http://purl.org/dc/elements/1.1/contributor"" />
+	  <Literal xml:lang=""EN"">Steve</Literal>
+	</Annotation>
+    <ObjectInverseOf>
+      <ObjectProperty IRI=""http://xmlns.com/foaf/0.1/knows"" />
+    </ObjectInverseOf>
+  </ReflexiveObjectProperty>
+</Ontology>");
+
+            Assert.IsNotNull(ontology);
+            Assert.IsTrue(ontology.ObjectPropertyAxioms.Count == 1);
+            Assert.IsTrue(ontology.ObjectPropertyAxioms.Single() is OWLReflexiveObjectProperty reflObjProp
+                            && reflObjProp.ObjectPropertyExpression is OWLObjectInverseOf objInvOf
+                            && string.Equals(objInvOf.ObjectProperty.IRI, RDFVocabulary.FOAF.KNOWS.ToString()));
+            Assert.IsTrue(ontology.ObjectPropertyAxioms.Single() is OWLReflexiveObjectProperty reflObjProp1
+                            && string.Equals(reflObjProp1.Annotations.Single().AnnotationProperty.IRI, "http://purl.org/dc/elements/1.1/contributor")
+                            && string.Equals(reflObjProp1.Annotations.Single().ValueLiteral.Value, "Steve")
+                            && string.Equals(reflObjProp1.Annotations.Single().ValueLiteral.Language, "EN"));
+        }
+        #endregion
+    }
+}
