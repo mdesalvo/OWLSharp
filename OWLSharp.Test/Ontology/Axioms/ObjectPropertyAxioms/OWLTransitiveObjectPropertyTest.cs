8e15ccd8d35003509be11bd955087e5a55f83afe
Marco De Salvo <marco.desalvo@vodafone.com>
2024-04-30 14:34:09 +0200

    .

--- OWLSharp.Test/Ontology/Axioms/ObjectPropertyAxioms/OWLTransitiveObjectPropertyTest.cs
+++ OWLSharp.Test/Ontology/Axioms/ObjectPropertyAxioms/OWLTransitiveObjectPropertyTest.cs
@@ -199,6 +199,33 @@ namespace OWLSharp.Ontology.Axioms.Test
                             && string.Equals(transObjProp1.Annotations.Single().ValueLiteral.Value, "Steve")
                             && string.Equals(transObjProp1.Annotations.Single().ValueLiteral.Language, "EN"));
         }
+
+        [TestMethod]
+        public void ShouldConvertTransitiveObjectPropertyToGraph()
+        {
+            OWLTransitiveObjectProperty transitiveObjectProperty = new OWLTransitiveObjectProperty(
+                new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS));
+            RDFGraph graph = transitiveObjectProperty.ToRDFGraph();
+
+            Assert.IsNotNull(graph);
+            Assert.IsTrue(graph.TriplesCount == 2);
+            Assert.IsTrue(graph[RDFVocabulary.FOAF.KNOWS, RDFVocabulary.RDF.TYPE, RDFVocabulary.OWL.OBJECT_PROPERTY, null].TriplesCount == 1);
+            Assert.IsTrue(graph[RDFVocabulary.FOAF.KNOWS, RDFVocabulary.RDF.TYPE, RDFVocabulary.OWL.TRANSITIVE_PROPERTY, null].TriplesCount == 1);
+        }
+
+        [TestMethod]
+        public void ShouldConvertTransitiveObjectInverseOfToGraph()
+        {
+            OWLTransitiveObjectProperty transitiveObjectProperty = new OWLTransitiveObjectProperty(
+                new OWLObjectInverseOf(new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)));
+            RDFGraph graph = transitiveObjectProperty.ToRDFGraph();
+
+            Assert.IsNotNull(graph);
+            Assert.IsTrue(graph.TriplesCount == 3);
+            Assert.IsTrue(graph[null, RDFVocabulary.RDF.TYPE, RDFVocabulary.OWL.TRANSITIVE_PROPERTY, null].TriplesCount == 1);
+            Assert.IsTrue(graph[null, RDFVocabulary.OWL.INVERSE_OF, RDFVocabulary.FOAF.KNOWS, null].TriplesCount == 1);
+            Assert.IsTrue(graph[RDFVocabulary.FOAF.KNOWS, RDFVocabulary.RDF.TYPE, RDFVocabulary.OWL.OBJECT_PROPERTY, null].TriplesCount == 1);
+        }
         #endregion
     }
 }
