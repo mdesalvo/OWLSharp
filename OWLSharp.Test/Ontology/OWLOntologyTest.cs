4e3a18bd2b7bf3ada534d4c37eca998706e2f1d8
mdesalvo <floating_point_9@msn.com>
2024-05-05 10:34:26 +0200

    .

--- OWLSharp.Test/Ontology/OWLOntologyTest.cs
+++ OWLSharp.Test/Ontology/OWLOntologyTest.cs
@@ -591,6 +591,7 @@ namespace OWLSharp.Ontology.Test
 
 			//Read from file and deserialize to test content
 			OWLOntology ontology2 = OWLSerializer.Deserialize(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "OWLOntologyTest_ShouldWriteOntologyToFile.owx")));
+
 			Assert.IsNotNull(ontology2);
 			Assert.IsTrue(string.Equals(ontology2.IRI, "ex:ont"));
 			Assert.IsTrue(string.Equals(ontology2.VersionIRI, "ex:ont/v1"));
@@ -621,6 +622,88 @@ namespace OWLSharp.Ontology.Test
 		[TestMethod]
 		public void ShouldThrowExceptionOnWritingOntologyToFileBecauseNullPath()
 			=> Assert.ThrowsException<OWLException>(() => new OWLOntology().ToFile(OWLEnums.OWLFormats.Owl2Xml, null));
+
+		[TestMethod]
+		public void ShouldWriteOntologyToStream()
+		{
+			OWLOntology ontology = new OWLOntology(new Uri("ex:ont"), new Uri("ex:ont/v1"));
+			ontology.Prefixes.Add(
+				new OWLPrefix(RDFNamespaceRegister.GetByPrefix(RDFVocabulary.FOAF.PREFIX)));
+			ontology.Imports.Add(
+				new OWLImport(new RDFResource("ex:ont2")));
+			ontology.Annotations.Add(
+				new OWLAnnotation(new OWLAnnotationProperty(RDFVocabulary.DC.DESCRIPTION), new OWLLiteral(new RDFPlainLiteral("annotation")))
+				{ 
+					Annotation = new OWLAnnotation(new OWLAnnotationProperty(RDFVocabulary.DC.DESCRIPTION), new OWLLiteral(new RDFPlainLiteral("nested annotation")))
+				});
+			ontology.DeclarationAxioms.AddRange(
+				[ new OWLDeclaration(new OWLClass(RDFVocabulary.FOAF.PERSON)), 
+				  new OWLDeclaration(new OWLClass(RDFVocabulary.FOAF.ORGANIZATION)), 
+				  new OWLDeclaration(new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)),
+				  new OWLDeclaration(new OWLDataProperty(RDFVocabulary.FOAF.AGE)),
+				  new OWLDeclaration(new OWLAnnotationProperty(RDFVocabulary.DC.DESCRIPTION)),
+				  new OWLDeclaration(new OWLAnnotationProperty(RDFVocabulary.RDFS.COMMENT)),
+				  new OWLDeclaration(new OWLNamedIndividual(new RDFResource("ex:Mark"))),
+				  new OWLDeclaration(new OWLNamedIndividual(new RDFResource("ex:Steve"))) ]);
+			ontology.ClassAxioms.Add(
+				new OWLDisjointClasses([ new OWLClass(RDFVocabulary.FOAF.PERSON), new OWLClass(RDFVocabulary.FOAF.ORGANIZATION) ]));
+			ontology.ObjectPropertyAxioms.Add(
+				new OWLAsymmetricObjectProperty(new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)));
+			ontology.DataPropertyAxioms.Add(
+				new OWLDataPropertyDomain(new OWLDataProperty(RDFVocabulary.FOAF.AGE), new OWLClass(RDFVocabulary.FOAF.PERSON)));
+			ontology.DatatypeDefinitionAxioms.Add(
+				new OWLDatatypeDefinition(
+					new OWLDatatype(new RDFResource("ex:length6to10")),
+					new OWLDatatypeRestriction(
+						new OWLDatatype(RDFVocabulary.XSD.STRING),
+						[ new OWLFacetRestriction(new OWLLiteral(new RDFTypedLiteral("6", RDFModelEnums.RDFDatatypes.XSD_INT)), OWLFacetRestriction.MIN_LENGTH),
+						  new OWLFacetRestriction(new OWLLiteral(new RDFTypedLiteral("10", RDFModelEnums.RDFDatatypes.XSD_INT)), OWLFacetRestriction.MAX_LENGTH) ])));
+			ontology.KeyAxioms.Add(
+				new OWLHasKey(
+					new OWLClass(RDFVocabulary.FOAF.AGENT),
+					[ new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS) ],
+					[ new OWLDataProperty(RDFVocabulary.FOAF.AGE) ]));
+			ontology.AssertionAxioms.Add(
+				new OWLObjectPropertyAssertion(new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS), new OWLNamedIndividual(new RDFResource("ex:Mark")), new OWLNamedIndividual(new RDFResource("ex:Steve"))));
+			ontology.AnnotationAxioms.Add(
+				new OWLAnnotationAssertion(new OWLAnnotationProperty(RDFVocabulary.RDFS.COMMENT),new RDFResource("ex:Mark"),new OWLLiteral(new RDFPlainLiteral("This is Mark"))));
+			
+			//Write to stream
+			MemoryStream stream = new MemoryStream();
+			ontology.ToStream(OWLEnums.OWLFormats.Owl2Xml, stream);
+
+			//Read from stream and deserialize to test content
+			string fileContent;
+            using (StreamReader reader = new StreamReader(new MemoryStream(stream.ToArray())))
+                fileContent = reader.ReadToEnd();
+			OWLOntology ontology2 = OWLSerializer.Deserialize(fileContent);
+
+			Assert.IsNotNull(ontology2);
+			Assert.IsTrue(string.Equals(ontology2.IRI, "ex:ont"));
+			Assert.IsTrue(string.Equals(ontology2.VersionIRI, "ex:ont/v1"));
+			Assert.IsNotNull(ontology2.Prefixes);
+			Assert.IsTrue(ontology2.Prefixes.Count == 11); //TODO: since we inject 5 default prefixes, there may be duplicates after deserialization
+			Assert.IsNotNull(ontology2.Imports);
+			Assert.IsTrue(ontology2.Imports.Count == 1);
+			Assert.IsNotNull(ontology2.Annotations);
+			Assert.IsTrue(ontology2.Annotations.Count == 1);
+			Assert.IsNotNull(ontology2.DeclarationAxioms);
+			Assert.IsTrue(ontology2.DeclarationAxioms.Count == 8);
+			Assert.IsNotNull(ontology2.ClassAxioms);
+			Assert.IsTrue(ontology2.ClassAxioms.Count == 1);
+			Assert.IsNotNull(ontology2.ObjectPropertyAxioms);
+			Assert.IsTrue(ontology2.ObjectPropertyAxioms.Count == 1);
+			Assert.IsNotNull(ontology2.DataPropertyAxioms);
+			Assert.IsTrue(ontology2.DataPropertyAxioms.Count == 1);
+			Assert.IsNotNull(ontology2.DatatypeDefinitionAxioms);
+			Assert.IsTrue(ontology2.DatatypeDefinitionAxioms.Count == 1);
+			Assert.IsNotNull(ontology2.KeyAxioms);
+			Assert.IsTrue(ontology2.KeyAxioms.Count == 1);
+			Assert.IsNotNull(ontology2.AssertionAxioms);
+			Assert.IsTrue(ontology2.AssertionAxioms.Count == 1);
+			Assert.IsNotNull(ontology2.AnnotationAxioms);
+			Assert.IsTrue(ontology2.AnnotationAxioms.Count == 1);			
+		}
 
 		[TestMethod]
 		public void ShouldThrowExceptionOnWritingOntologyToStreamBecauseNullStream()
