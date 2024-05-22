39c68535cc44e3c0cbe836cc951cbf116e4a61b7
Marco De Salvo <marco.desalvo@vodafone.com>
2024-05-22 10:22:11 +0200

    Begin work on Navigator namespace

---
+++ OWLSharp.Test/Navigator/OWLEntityHelperTest.cs
@@ -0,0 +1,53 @@
+﻿/*
+   Copyright 2014-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OWLSharp.Modeler;
+using OWLSharp.Modeler.Axioms;
+using OWLSharp.Modeler.Expressions;
+using OWLSharp.Navigator;
+using RDFSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OWLSharp.Test.Navigator
+{
+    [TestClass]
+    public class OWLEntityHelperTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldGetDeclaredClasses()
+        {
+            OWLOntology ont = new OWLOntology(new Uri("ex:ont"))
+            {
+                DeclarationAxioms = [
+                    new OWLDeclaration(new OWLClass(RDFVocabulary.FOAF.AGENT)),
+                    new OWLDeclaration(new OWLClass(RDFVocabulary.FOAF.ORGANIZATION)),
+                    new OWLDeclaration(new OWLClass(RDFVocabulary.FOAF.PERSON))
+                ]
+            };
+            List<RDFResource> cls = ont.GetDeclaredClasses();
+
+            Assert.IsTrue(cls.Count == 3);
+            Assert.IsTrue(cls.Any(c => c.Equals(RDFVocabulary.FOAF.AGENT)));
+            Assert.IsTrue(cls.Any(c => c.Equals(RDFVocabulary.FOAF.ORGANIZATION)));
+            Assert.IsTrue(cls.Any(c => c.Equals(RDFVocabulary.FOAF.PERSON)));
+        }
+        #endregion
+    }
+}
---
+++ OWLSharp/Navigator/OWLEntityHelper.cs
@@ -0,0 +1,35 @@
+﻿/*
+   Copyright 2014-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using OWLSharp.Modeler;
+using OWLSharp.Modeler.Expressions;
+using RDFSharp.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OWLSharp.Navigator
+{
+    public static class OWLEntityHelper
+    {
+        #region Methods
+        public static List<RDFResource> GetDeclaredClasses(this OWLOntology ontology)
+            => ontology?.DeclarationAxioms.Where(dax => dax.Expression is OWLClass daxCls)
+                                          .Select(dax => ((OWLClass)dax.Expression).GetIRI())
+                                          .ToList() 
+                ?? Enumerable.Empty<RDFResource>().ToList();
+        #endregion
+    }
+}
