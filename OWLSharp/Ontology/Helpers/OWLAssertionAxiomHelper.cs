895582e422a90ca57db8a01da39c4c694b59e795
mdesalvo <floating_point_9@msn.com>
2024-06-21 20:56:16 +0200

    .

--- OWLSharp/Ontology/Helpers/OWLAssertionAxiomHelper.cs
+++ OWLSharp/Ontology/Helpers/OWLAssertionAxiomHelper.cs
@@ -17,7 +17,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using OWLSharp.Ontology.Axioms;
 using OWLSharp.Ontology.Expressions;
 using RDFSharp.Model;
