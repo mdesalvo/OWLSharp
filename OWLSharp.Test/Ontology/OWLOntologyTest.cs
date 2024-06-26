11a33cc15efb82bff5885a3e20f9d97b626c7fd0
Marco De Salvo <marco.desalvo@vodafone.com>
2024-06-26 15:25:31 +0200

    .

--- OWLSharp.Test/Ontology/OWLOntologyTest.cs
+++ OWLSharp.Test/Ontology/OWLOntologyTest.cs
@@ -17,8 +17,6 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OWLSharp.Ontology;
