5f328225dd9f8637255edddfc5bb7e3f3a6728aa
mdesalvo <floating_point_9@msn.com>
2024-07-07 10:24:07 +0200

    .

--- OWLSharp.Test/Reasoner/Rules/OWLClassAssertionEntailmentRuleTest.cs
+++ OWLSharp.Test/Reasoner/Rules/OWLClassAssertionEntailmentRuleTest.cs
@@ -102,7 +102,7 @@ namespace OWLSharp.Test.Reasoner.Rules
         }
 
 		[TestMethod]
-        public void ShouldEntailClassAssertionOnSubClassOfHavingEquivalentCase()
+        public void ShouldEntailClassAssertionOnSubClassOfHavingEquivalentClassCase()
         {
             OWLOntology ontology = new OWLOntology()
             {
@@ -119,6 +119,44 @@ namespace OWLSharp.Test.Reasoner.Rules
 					new OWLEquivalentClasses([
 						new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/DomesticFeline")),
 						new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/MewAnimal"))])
+                ],
+                AssertionAxioms = [
+                    new OWLClassAssertion(
+                        new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/Cat")),
+                        new OWLNamedIndividual(new RDFResource("http://xmlns.com/foaf/0.1/Felix")))
+                ]
+            };
+            List<OWLAxiom> inferences = OWLClassAssertionEntailmentRule.ExecuteRule(ontology);
+
+            Assert.IsNotNull(inferences);
+            Assert.IsTrue(inferences.TrueForAll(inf => inf.IsInference));
+            Assert.IsTrue(inferences.Count == 2);
+            Assert.IsTrue(inferences[0] is OWLClassAssertion inf 
+                            && string.Equals(inf.ClassExpression.GetIRI().ToString(), "http://xmlns.com/foaf/0.1/MewAnimal")
+                            && string.Equals(inf.IndividualExpression.GetIRI().ToString(), "http://xmlns.com/foaf/0.1/Felix"));
+			Assert.IsTrue(inferences[1] is OWLClassAssertion inf1 
+                            && string.Equals(inf1.ClassExpression.GetIRI().ToString(), "http://xmlns.com/foaf/0.1/DomesticFeline")
+                            && string.Equals(inf1.IndividualExpression.GetIRI().ToString(), "http://xmlns.com/foaf/0.1/Felix"));
+        }
+
+		[TestMethod]
+        public void ShouldEntailClassAssertionOnEquivalentClassHavingSubClassOfCase()
+        {
+            OWLOntology ontology = new OWLOntology()
+            {
+                DeclarationAxioms = [ 
+					new OWLDeclaration(new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/MewAnimal"))),
+                    new OWLDeclaration(new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/DomesticFeline"))),
+					new OWLDeclaration(new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/Cat"))),
+                    new OWLDeclaration(new OWLNamedIndividual(new RDFResource("http://xmlns.com/foaf/0.1/Felix")))
+                ],
+                ClassAxioms = [ 
+                    new OWLSubClassOf(
+						new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/MewAnimal")),
+						new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/DomesticFeline"))),
+					new OWLEquivalentClasses([
+						new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/MewAnimal")),
+						new OWLClass(new RDFResource("http://xmlns.com/foaf/0.1/Cat"))])
                 ],
                 AssertionAxioms = [
                     new OWLClassAssertion(
