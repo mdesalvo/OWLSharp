bdef1bfcfad62b76949387eb6328c1a9c1e960a7
mdesalvo <floating_point_9@msn.com>
2024-07-13 11:37:35 +0200

    .

--- OWLSharp/Reasoner/Rules/OWLHasKeyEntailmentRule.cs
+++ OWLSharp/Reasoner/Rules/OWLHasKeyEntailmentRule.cs
@@ -38,10 +38,8 @@ namespace OWLSharp.Reasoner.Rules
 				List<OWLIndividualExpression> hasKeyClassIdvs = ontology.GetIndividualsOf(hasKeyAxiom.ClassExpression);
 
 				//HasKey(C, OP) ^ ObjectPropertyAssertion(OP, I1, IX) ^ ObjectPropertyAssertion(OP, I2, IX) -> SameIndividual(I1,I2)
-				inferences.AddRange(AnalyzeObjectKeyValues(hasKeyAxiom, hasKeyClassIdvs, opAsns));
-
 				//HasKey(C, DP) ^ DataPropertyAssertion(DP, I1, LIT) ^ DataPropertyAssertion(DP, I2, LIT) -> SameIndividual(I1,I2)
-				inferences.AddRange(AnalyzeDataKeyValues(hasKeyAxiom, hasKeyClassIdvs, dpAsns));
+				inferences.AddRange(AnalyzeKeyValues(hasKeyAxiom, hasKeyClassIdvs, opAsns, dpAsns));
 			}
 
 			//Remove inferences already stated in explicit knowledge
@@ -50,12 +48,14 @@ namespace OWLSharp.Reasoner.Rules
             return OWLAxiomHelper.RemoveDuplicates(inferences);
         }
 
-		private static List<OWLAxiom> AnalyzeObjectKeyValues(OWLHasKey hasKeyAxiom, List<OWLIndividualExpression> hasKeyClassIdvs, List<OWLObjectPropertyAssertion> opAsns)
+		private static List<OWLAxiom> AnalyzeKeyValues(OWLHasKey hasKeyAxiom, List<OWLIndividualExpression> hasKeyClassIdvs, 
+			List<OWLObjectPropertyAssertion> opAsns, List<OWLDataPropertyAssertion> dpAsns)
 		{
 			List<OWLAxiom> inferences = new List<OWLAxiom>();
 
 			//Temporary working variables
 			Dictionary<string, List<OWLIndividualExpression>> objectKeyValueRegister = new Dictionary<string, List<OWLIndividualExpression>>();
+			Dictionary<string, List<OWLIndividualExpression>> dataKeyValueRegister = new Dictionary<string, List<OWLIndividualExpression>>();
 			OWLIndividualExpression swapIdvExpr;
 
 			#region Compute Keys
@@ -64,106 +64,97 @@ namespace OWLSharp.Reasoner.Rules
 			{
 				RDFResource idvExprIRI = idvExpr.GetIRI();
 
-				//Calculate the key values of the current individual
-                StringBuilder sb = new StringBuilder();
-                foreach (OWLObjectPropertyExpression keyObjectProperty in hasKeyAxiom.ObjectPropertyExpressions)
-                {
-					OWLObjectProperty keyObjectPropertyInvOfValue = (keyObjectProperty as OWLObjectInverseOf)?.ObjectProperty;
-
-					#region Calibration
-					List<OWLObjectPropertyAssertion> keyObjectPropertyAsns = OWLAssertionAxiomHelper.SelectObjectAssertionsByOPEX(opAsns, keyObjectProperty);
-                    for (int i=0; i<keyObjectPropertyAsns.Count; i++)
+				#region Object Keys
+				if (hasKeyAxiom.ObjectPropertyExpressions.Count > 0)
+				{
+					//Calculate the key values of the current individual
+					StringBuilder objSB = new StringBuilder();
+					foreach (OWLObjectPropertyExpression keyObjectProperty in hasKeyAxiom.ObjectPropertyExpressions)
 					{
-						//In case the object assertion works under inverse logic, we must swap source/target of the object assertion
-						if (keyObjectPropertyAsns[i].ObjectPropertyExpression is OWLObjectInverseOf objInvOf)
-						{   
-							swapIdvExpr = keyObjectPropertyAsns[i].SourceIndividualExpression;
-							keyObjectPropertyAsns[i].SourceIndividualExpression = keyObjectPropertyAsns[i].TargetIndividualExpression;
-							keyObjectPropertyAsns[i].TargetIndividualExpression = swapIdvExpr;
-							keyObjectPropertyAsns[i].ObjectPropertyExpression = objInvOf.ObjectProperty;
-						}
+						OWLObjectProperty keyObjectPropertyInvOfValue = (keyObjectProperty as OWLObjectInverseOf)?.ObjectProperty;
 
-						//In case the key object property works under inverse logic, we must swap source/target of the object assertion
-						if (keyObjectPropertyInvOfValue != null)
+						#region Calibration
+						List<OWLObjectPropertyAssertion> keyObjectPropertyAsns = OWLAssertionAxiomHelper.SelectObjectAssertionsByOPEX(opAsns, keyObjectProperty);
+						for (int i=0; i<keyObjectPropertyAsns.Count; i++)
 						{
-							swapIdvExpr = keyObjectPropertyAsns[i].SourceIndividualExpression;
-							keyObjectPropertyAsns[i].SourceIndividualExpression = keyObjectPropertyAsns[i].TargetIndividualExpression;
-							keyObjectPropertyAsns[i].TargetIndividualExpression = swapIdvExpr;
-							keyObjectPropertyAsns[i].ObjectPropertyExpression = keyObjectPropertyInvOfValue;
+							//In case the object assertion works under inverse logic, we must swap source/target of the object assertion
+							if (keyObjectPropertyAsns[i].ObjectPropertyExpression is OWLObjectInverseOf objInvOf)
+							{   
+								swapIdvExpr = keyObjectPropertyAsns[i].SourceIndividualExpression;
+								keyObjectPropertyAsns[i].SourceIndividualExpression = keyObjectPropertyAsns[i].TargetIndividualExpression;
+								keyObjectPropertyAsns[i].TargetIndividualExpression = swapIdvExpr;
+								keyObjectPropertyAsns[i].ObjectPropertyExpression = objInvOf.ObjectProperty;
+							}
+
+							//In case the key object property works under inverse logic, we must swap source/target of the object assertion
+							if (keyObjectPropertyInvOfValue != null)
+							{
+								swapIdvExpr = keyObjectPropertyAsns[i].SourceIndividualExpression;
+								keyObjectPropertyAsns[i].SourceIndividualExpression = keyObjectPropertyAsns[i].TargetIndividualExpression;
+								keyObjectPropertyAsns[i].TargetIndividualExpression = swapIdvExpr;
+								keyObjectPropertyAsns[i].ObjectPropertyExpression = keyObjectPropertyInvOfValue;
+							}
 						}
+						keyObjectPropertyAsns = OWLAxiomHelper.RemoveDuplicates(keyObjectPropertyAsns);
+						#endregion
+
+						if (keyObjectPropertyAsns.Count > 0)
+							objSB.Append(string.Join("§§", keyObjectPropertyAsns.Where(asn => asn.SourceIndividualExpression.GetIRI().Equals(idvExprIRI))
+																				.Select(asn => asn.TargetIndividualExpression.GetIRI().ToString())));
 					}
-					keyObjectPropertyAsns = OWLAxiomHelper.RemoveDuplicates(keyObjectPropertyAsns);
-					#endregion
 
-					if (keyObjectPropertyAsns.Count > 0)
-                        sb.Append(string.Join("§§", keyObjectPropertyAsns.Where(asn => asn.SourceIndividualExpression.GetIRI().Equals(idvExprIRI))
-																		 .Select(asn => asn.TargetIndividualExpression.GetIRI().ToString())));
-                }
+					//Collect the key values of the current individual into the register
+					string objSBValue = objSB.ToString();
+					if (!string.IsNullOrEmpty(objSBValue))
+					{
+						if (!objectKeyValueRegister.ContainsKey(objSBValue))
+							objectKeyValueRegister.Add(objSBValue, new List<OWLIndividualExpression>());
+						objectKeyValueRegister[objSBValue].Add(idvExpr);
+					}
+				}				
+				#endregion
 
-                //Collect the key values of the current individual into the register
-                string sbValue = sb.ToString();
-                if (!string.IsNullOrEmpty(sbValue))
-                {
-                    if (!objectKeyValueRegister.ContainsKey(sbValue))
-                        objectKeyValueRegister.Add(sbValue, new List<OWLIndividualExpression>());
-                    objectKeyValueRegister[sbValue].Add(idvExpr);
-                }
+				#region Data Keys
+				if (hasKeyAxiom.DataProperties.Count > 0)
+				{
+					//Calculate the key values of the current individual
+					StringBuilder dtSB = new StringBuilder();
+					foreach (OWLDataProperty keyDataProperty in hasKeyAxiom.DataProperties)
+					{
+						List<OWLDataPropertyAssertion> keyDataPropertyAsns = OWLAssertionAxiomHelper.SelectDataAssertionsByDPEX(dpAsns, keyDataProperty);
+						if (keyDataPropertyAsns.Count > 0)
+							dtSB.Append(string.Join("§§", keyDataPropertyAsns.Where(asn => asn.IndividualExpression.GetIRI().Equals(idvExprIRI))
+																			 .Select(asn => asn.Literal.GetLiteral().ToString())));
+					}
+
+					//Collect the key values of the current individual into the register
+					string dtSBValue = dtSB.ToString();
+					if (!string.IsNullOrEmpty(dtSBValue))
+					{
+						if (!dataKeyValueRegister.ContainsKey(dtSBValue))
+							dataKeyValueRegister.Add(dtSBValue, new List<OWLIndividualExpression>());
+						dataKeyValueRegister[dtSBValue].Add(idvExpr);
+					}
+				}
+				#endregion
 			}
 			#endregion
 
 			#region Analyze Keys
-			//Restrict analysis to keys which have been generated by 2+ individuals
+			//Restrict analysis to object keys which have been generated by 2+ individuals
+			//HasKey(C, OP) ^ ObjectPropertyAssertion(OP, I1, IX) ^ ObjectPropertyAssertion(OP, I2, IX) -> SameIndividual(I1,I2)
             foreach (KeyValuePair<string, List<OWLIndividualExpression>> collisionKeyValueRegister in objectKeyValueRegister.Where(kvr => kvr.Value.Count > 1)
                                                                                                             				.ToDictionary(kv => kv.Key, kv => kv.Value))
 				inferences.Add(new OWLSameIndividual(collisionKeyValueRegister.Value) { IsInference=true });
-			#endregion
 
-			return inferences;
-		}
-    
-		private static List<OWLAxiom> AnalyzeDataKeyValues(OWLHasKey hasKeyAxiom, List<OWLIndividualExpression> hasKeyClassIdvs, List<OWLDataPropertyAssertion> dpAsns)
-		{
-			List<OWLAxiom> inferences = new List<OWLAxiom>();
-
-			//Temporary working variables
-			Dictionary<string, List<OWLIndividualExpression>> dataKeyValueRegister = new Dictionary<string, List<OWLIndividualExpression>>();
-
-			#region Compute Keys
-			//Iterate individuals of the HasKey axiom's class in order to calculate their key values
-			foreach (OWLIndividualExpression idvExpr in hasKeyClassIdvs)
-			{
-				RDFResource idvExprIRI = idvExpr.GetIRI();
-
-				//Calculate the key values of the current individual
-                StringBuilder sb = new StringBuilder();
-                foreach (OWLDataProperty keyDataProperty in hasKeyAxiom.DataProperties)
-                {
-					List<OWLDataPropertyAssertion> keyDataPropertyAsns = OWLAssertionAxiomHelper.SelectDataAssertionsByDPEX(dpAsns, keyDataProperty);
-					if (keyDataPropertyAsns.Count > 0)
-                        sb.Append(string.Join("§§", keyDataPropertyAsns.Where(asn => asn.IndividualExpression.GetIRI().Equals(idvExprIRI))
-																	   .Select(asn => asn.Literal.GetLiteral().ToString())));
-                }
-
-                //Collect the key values of the current individual into the register
-                string sbValue = sb.ToString();
-                if (!string.IsNullOrEmpty(sbValue))
-                {
-                    if (!dataKeyValueRegister.ContainsKey(sbValue))
-                        dataKeyValueRegister.Add(sbValue, new List<OWLIndividualExpression>());
-                    dataKeyValueRegister[sbValue].Add(idvExpr);
-                }
-			}
-			#endregion
-
-			#region Analyze Keys
-			//Restrict analysis to keys which have been generated by 2+ individuals
-            foreach (KeyValuePair<string, List<OWLIndividualExpression>> collisionKeyValueRegister in dataKeyValueRegister.Where(kvr => kvr.Value.Count > 1)
+			//Restrict analysis to data keys which have been generated by 2+ individuals
+			//HasKey(C, DP) ^ DataPropertyAssertion(DP, I1, LIT) ^ DataPropertyAssertion(DP, I2, LIT) -> SameIndividual(I1,I2)
+			foreach (KeyValuePair<string, List<OWLIndividualExpression>> collisionKeyValueRegister in dataKeyValueRegister.Where(kvr => kvr.Value.Count > 1)
                                                                                                             			  .ToDictionary(kv => kv.Key, kv => kv.Value))
 				inferences.Add(new OWLSameIndividual(collisionKeyValueRegister.Value) { IsInference=true });
 			#endregion
 
 			return inferences;
 		}
-    
 	}
 }
