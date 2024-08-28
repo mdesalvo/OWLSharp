e5a56c5c4a4584c04907444cabfc98b765937273
Marco De Salvo <marco.desalvo@vodafone.com>
2024-08-28 14:33:31 +0200

    .

--- OWLSharp/Ontology/Rules/SWRLBuiltInFactory.cs
+++ OWLSharp/Ontology/Rules/SWRLBuiltInFactory.cs
@@ -18,6 +18,7 @@ using OWLSharp.Ontology.Expressions;
 using OWLSharp.Ontology.Rules.Arguments;
 using OWLSharp.Ontology.Rules.Atoms;
 using RDFSharp.Model;
+using RDFSharp.Query;
 
 namespace OWLSharp.Ontology.Rules
 {
@@ -226,6 +227,158 @@ namespace OWLSharp.Ontology.Rules
                 Predicate = new OWLExpression() { ExpressionIRI = new RDFResource("http://www.w3.org/2003/11/swrlb#tan") },
                 LeftArgument = leftArgument,
                 RightArgument = rightArgument
+            };
+        }
+
+        //Comparison
+
+        public static SWRLBuiltInAtom Equal(SWRLVariableArgument leftArgument, SWRLVariableArgument rightArgument)
+            => EqualInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom Equal(SWRLVariableArgument leftArgument, SWRLIndividualArgument rightArgument)
+           => EqualInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom Equal(SWRLVariableArgument leftArgument, SWRLLiteralArgument rightArgument)
+           => EqualInternal(leftArgument, rightArgument);
+        internal static SWRLBuiltInAtom EqualInternal(SWRLArgument leftArgument, SWRLArgument rightArgument)
+        {
+            #region Guards
+            if (rightArgument == null)
+                throw new OWLException("Cannot create built-in because given \"rightArgument\" parameter is null");
+            #endregion
+
+            return new SWRLBuiltInAtom()
+            {
+                Predicate = new OWLExpression() { ExpressionIRI = new RDFResource("http://www.w3.org/2003/11/swrlb#equal") },
+                LeftArgument = leftArgument,
+                RightArgument = rightArgument,
+                FilterValue = new RDFComparisonFilter(
+                    RDFQueryEnums.RDFComparisonFlavors.EqualTo,
+                    RDFQueryUtilities.ParseRDFPatternMember(leftArgument.ToString()),
+                    RDFQueryUtilities.ParseRDFPatternMember(rightArgument.ToString()))
+            };
+        }
+
+        public static SWRLBuiltInAtom GreaterThan(SWRLVariableArgument leftArgument, SWRLVariableArgument rightArgument)
+            => GreaterThanInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom GreaterThan(SWRLVariableArgument leftArgument, SWRLIndividualArgument rightArgument)
+           => GreaterThanInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom GreaterThan(SWRLVariableArgument leftArgument, SWRLLiteralArgument rightArgument)
+           => GreaterThanInternal(leftArgument, rightArgument);
+        internal static SWRLBuiltInAtom GreaterThanInternal(SWRLArgument leftArgument, SWRLArgument rightArgument)
+        {
+            #region Guards
+            if (rightArgument == null)
+                throw new OWLException("Cannot create built-in because given \"rightArgument\" parameter is null");
+            #endregion
+
+            return new SWRLBuiltInAtom()
+            {
+                Predicate = new OWLExpression() { ExpressionIRI = new RDFResource("http://www.w3.org/2003/11/swrlb#greaterThan") },
+                LeftArgument = leftArgument,
+                RightArgument = rightArgument,
+                FilterValue = new RDFComparisonFilter(
+                    RDFQueryEnums.RDFComparisonFlavors.GreaterThan,
+                    RDFQueryUtilities.ParseRDFPatternMember(leftArgument.ToString()),
+                    RDFQueryUtilities.ParseRDFPatternMember(rightArgument.ToString()))
+            };
+        }
+
+        public static SWRLBuiltInAtom GreaterThanOrEqual(SWRLVariableArgument leftArgument, SWRLVariableArgument rightArgument)
+            => GreaterThanOrEqualInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom GreaterThanOrEqual(SWRLVariableArgument leftArgument, SWRLIndividualArgument rightArgument)
+           => GreaterThanOrEqualInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom GreaterThanOrEqual(SWRLVariableArgument leftArgument, SWRLLiteralArgument rightArgument)
+           => GreaterThanOrEqualInternal(leftArgument, rightArgument);
+        internal static SWRLBuiltInAtom GreaterThanOrEqualInternal(SWRLArgument leftArgument, SWRLArgument rightArgument)
+        {
+            #region Guards
+            if (rightArgument == null)
+                throw new OWLException("Cannot create built-in because given \"rightArgument\" parameter is null");
+            #endregion
+
+            return new SWRLBuiltInAtom()
+            {
+                Predicate = new OWLExpression() { ExpressionIRI = new RDFResource("http://www.w3.org/2003/11/swrlb#greaterThanOrEqual") },
+                LeftArgument = leftArgument,
+                RightArgument = rightArgument,
+                FilterValue = new RDFComparisonFilter(
+                    RDFQueryEnums.RDFComparisonFlavors.GreaterOrEqualThan,
+                    RDFQueryUtilities.ParseRDFPatternMember(leftArgument.ToString()),
+                    RDFQueryUtilities.ParseRDFPatternMember(rightArgument.ToString()))
+            };
+        }
+
+        public static SWRLBuiltInAtom LessThan(SWRLVariableArgument leftArgument, SWRLVariableArgument rightArgument)
+            => LessThanInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom LessThan(SWRLVariableArgument leftArgument, SWRLIndividualArgument rightArgument)
+           => LessThanInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom LessThan(SWRLVariableArgument leftArgument, SWRLLiteralArgument rightArgument)
+           => LessThanInternal(leftArgument, rightArgument);
+        internal static SWRLBuiltInAtom LessThanInternal(SWRLArgument leftArgument, SWRLArgument rightArgument)
+        {
+            #region Guards
+            if (rightArgument == null)
+                throw new OWLException("Cannot create built-in because given \"rightArgument\" parameter is null");
+            #endregion
+
+            return new SWRLBuiltInAtom()
+            {
+                Predicate = new OWLExpression() { ExpressionIRI = new RDFResource("http://www.w3.org/2003/11/swrlb#lessThan") },
+                LeftArgument = leftArgument,
+                RightArgument = rightArgument,
+                FilterValue = new RDFComparisonFilter(
+                    RDFQueryEnums.RDFComparisonFlavors.LessThan,
+                    RDFQueryUtilities.ParseRDFPatternMember(leftArgument.ToString()),
+                    RDFQueryUtilities.ParseRDFPatternMember(rightArgument.ToString()))
+            };
+        }
+
+        public static SWRLBuiltInAtom LessThanOrEqual(SWRLVariableArgument leftArgument, SWRLVariableArgument rightArgument)
+            => LessThanOrEqualInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom LessThanOrEqual(SWRLVariableArgument leftArgument, SWRLIndividualArgument rightArgument)
+           => LessThanOrEqualInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom LessThanOrEqual(SWRLVariableArgument leftArgument, SWRLLiteralArgument rightArgument)
+           => LessThanOrEqualInternal(leftArgument, rightArgument);
+        internal static SWRLBuiltInAtom LessThanOrEqualInternal(SWRLArgument leftArgument, SWRLArgument rightArgument)
+        {
+            #region Guards
+            if (rightArgument == null)
+                throw new OWLException("Cannot create built-in because given \"rightArgument\" parameter is null");
+            #endregion
+
+            return new SWRLBuiltInAtom()
+            {
+                Predicate = new OWLExpression() { ExpressionIRI = new RDFResource("http://www.w3.org/2003/11/swrlb#lessThanOrEqual") },
+                LeftArgument = leftArgument,
+                RightArgument = rightArgument,
+                FilterValue = new RDFComparisonFilter(
+                    RDFQueryEnums.RDFComparisonFlavors.LessOrEqualThan,
+                    RDFQueryUtilities.ParseRDFPatternMember(leftArgument.ToString()),
+                    RDFQueryUtilities.ParseRDFPatternMember(rightArgument.ToString()))
+            };
+        }
+
+        public static SWRLBuiltInAtom NotEqual(SWRLVariableArgument leftArgument, SWRLVariableArgument rightArgument)
+            => NotEqualInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom NotEqual(SWRLVariableArgument leftArgument, SWRLIndividualArgument rightArgument)
+           => NotEqualInternal(leftArgument, rightArgument);
+        public static SWRLBuiltInAtom NotEqual(SWRLVariableArgument leftArgument, SWRLLiteralArgument rightArgument)
+           => NotEqualInternal(leftArgument, rightArgument);
+        internal static SWRLBuiltInAtom NotEqualInternal(SWRLArgument leftArgument, SWRLArgument rightArgument)
+        {
+            #region Guards
+            if (rightArgument == null)
+                throw new OWLException("Cannot create built-in because given \"rightArgument\" parameter is null");
+            #endregion
+
+            return new SWRLBuiltInAtom()
+            {
+                Predicate = new OWLExpression() { ExpressionIRI = new RDFResource("http://www.w3.org/2003/11/swrlb#notEqual") },
+                LeftArgument = leftArgument,
+                RightArgument = rightArgument,
+                FilterValue = new RDFComparisonFilter(
+                    RDFQueryEnums.RDFComparisonFlavors.NotEqualTo,
+                    RDFQueryUtilities.ParseRDFPatternMember(leftArgument.ToString()),
+                    RDFQueryUtilities.ParseRDFPatternMember(rightArgument.ToString()))
             };
         }
 
