3b8fc2b946aa9d3050371c97bdb2e70558822d5a
Marco De Salvo <marco.desalvo@vodafone.com>
2024-08-29 10:02:18 +0200

    .

--- OWLSharp.Test/Ontology/Rules/SWRLArgumentTest.cs
+++ OWLSharp.Test/Ontology/Rules/SWRLArgumentTest.cs
@@ -39,6 +39,41 @@ namespace OWLSharp.Test.Ontology.Rules
             Assert.ThrowsException<OWLException>(() => new SWRLIndividualArgument(new RDFResource()));
             Assert.ThrowsException<OWLException>(() => new SWRLIndividualArgument(new RDFResource("bnode:jd8d7f")));
         }
+
+        [TestMethod]
+        public void ShouldCreateSWRLLiteralArgument()
+        {
+            SWRLLiteralArgument argPL = new SWRLLiteralArgument(new RDFPlainLiteral("hello"));
+
+            Assert.IsNotNull(argPL);
+            Assert.IsTrue(string.Equals("hello", argPL.Value));
+            Assert.IsNull(argPL.Language);
+            Assert.IsNull(argPL.DatatypeIRI);
+            Assert.IsTrue(string.Equals("hello", argPL.ToString()));
+            Assert.IsTrue(argPL.GetLiteral().Equals(new RDFPlainLiteral("hello")));
+            Assert.IsTrue(string.Equals("<Literal>hello</Literal>", OWLSerializer.SerializeObject(argPL)));
+
+            SWRLLiteralArgument argPLL = new SWRLLiteralArgument(new RDFPlainLiteral("hello", "en"));
+
+            Assert.IsNotNull(argPLL);
+            Assert.IsTrue(string.Equals("hello", argPLL.Value));
+            Assert.IsTrue(string.Equals("EN", argPLL.Language));
+            Assert.IsNull(argPLL.DatatypeIRI);
+            Assert.IsTrue(string.Equals("hello@EN", argPLL.ToString()));
+            Assert.IsTrue(argPLL.GetLiteral().Equals(new RDFPlainLiteral("hello", "en")));
+            Assert.IsTrue(string.Equals("<Literal xml:lang=\"EN\">hello</Literal>", OWLSerializer.SerializeObject(argPLL)));
+
+            SWRLLiteralArgument argPLT = new SWRLLiteralArgument(new RDFTypedLiteral("hello", RDFModelEnums.RDFDatatypes.XSD_STRING));
+
+            Assert.IsNotNull(argPLT);
+            Assert.IsTrue(string.Equals("hello", argPLT.Value));
+            Assert.IsNull(argPLT.Language);
+            Assert.IsTrue(string.Equals("http://www.w3.org/2001/XMLSchema#string", argPLT.DatatypeIRI));
+            Assert.IsTrue(string.Equals("hello^^http://www.w3.org/2001/XMLSchema#string", argPLT.ToString()));
+            Assert.IsTrue(argPLT.GetLiteral().Equals(new RDFTypedLiteral("hello", RDFModelEnums.RDFDatatypes.XSD_STRING)));
+            Assert.IsTrue(string.Equals("<Literal datatypeIRI=\"http://www.w3.org/2001/XMLSchema#string\">hello</Literal>", OWLSerializer.SerializeObject(argPLT)));
+
+        }
         #endregion
     }
 }
