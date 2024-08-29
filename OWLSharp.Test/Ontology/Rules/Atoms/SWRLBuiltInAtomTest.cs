ba56c308f2aa488e1c7262fdb975cb3d9a8a9757
Marco De Salvo <marco.desalvo@vodafone.com>
2024-08-29 10:23:23 +0200

    .

---
+++ OWLSharp.Test/Ontology/Rules/Atoms/SWRLBuiltInAtomTest.cs
@@ -0,0 +1,57 @@
+﻿/*
+   Copyright 2014-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OWLSharp.Ontology;
+using OWLSharp.Ontology.Rules;
+using RDFSharp.Query;
+
+namespace OWLSharp.Test.Ontology.Rules
+{
+    [TestClass]
+    public class SWRLBuiltInAtomTest
+    {
+        #region Tests
+        [TestMethod]
+        public void ShouldCreateAbsBuiltInAtom()
+        {
+            SWRLBuiltInAtom atom = SWRLBuiltInFactory.Abs(
+                new SWRLVariableArgument(new RDFVariable("?X")),
+                new SWRLVariableArgument(new RDFVariable("?Y")));
+
+            Assert.IsNotNull(atom);
+            Assert.IsTrue(atom.IsMathBuiltIn);
+            Assert.IsFalse(atom.IsComparisonFilterBuiltIn);
+            Assert.IsFalse(atom.IsStringFilterBuiltIn);
+            Assert.IsTrue(string.Equals("http://www.w3.org/2003/11/swrlb#abs", atom.IRI));
+            Assert.IsNull(atom.MathLiteral);
+            Assert.IsFalse(atom.ShouldSerializePredicate());
+            Assert.IsNull(atom.MathValue);
+            Assert.IsNull(atom.FilterValue);
+            Assert.IsNotNull(atom.Predicate);
+            Assert.IsTrue(string.Equals("http://www.w3.org/2003/11/swrlb#abs", atom.Predicate.GetIRI().ToString()));
+            Assert.IsNotNull(atom.LeftArgument);
+            Assert.IsTrue(atom.LeftArgument is SWRLVariableArgument vlarg && vlarg.GetVariable().Equals(new RDFVariable("?X")));
+            Assert.IsNotNull(atom.RightArgument);
+            Assert.IsTrue(atom.RightArgument is SWRLVariableArgument rlarg && rlarg.GetVariable().Equals(new RDFVariable("?Y")));
+            Assert.IsTrue(string.Equals("swrlb:abs(?X,?Y)", atom.ToString()));
+            Assert.IsTrue(string.Equals("<BuiltInAtom IRI=\"http://www.w3.org/2003/11/swrlb#abs\"><Variable IRI=\"urn:swrl:var#X\" /><Variable IRI=\"urn:swrl:var#Y\" /></BuiltInAtom>", OWLSerializer.SerializeObject(atom)));
+            Assert.ThrowsException<OWLException>(() => SWRLBuiltInFactory.Abs(null,new SWRLVariableArgument(new RDFVariable("?Y"))));
+            Assert.ThrowsException<OWLException>(() => SWRLBuiltInFactory.Abs(new SWRLVariableArgument(new RDFVariable("?X")), null));
+        }
+        #endregion
+    }
+}
