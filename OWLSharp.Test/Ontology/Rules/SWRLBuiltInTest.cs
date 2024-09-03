a0232b6b26f1c41621795b7541a86cf1575836cc
Marco De Salvo <marco.desalvo@vodafone.com>
2024-09-03 15:19:25 +0200

    .

--- OWLSharp.Test/Ontology/Rules/SWRLBuiltInTest.cs
+++ OWLSharp.Test/Ontology/Rules/SWRLBuiltInTest.cs
@@ -2708,6 +2708,94 @@ namespace OWLSharp.Test.Ontology.Rules
             Assert.IsTrue(builtinResults2.Rows.Count == 5); //SPARQL evaluates true every row if the column is unknown
         }
 
+        [TestMethod]
+        public void ShouldCreateEndsWithBuiltIn()
+        {
+            SWRLBuiltIn builtin = SWRLBuiltIn.EndsWith(
+                new SWRLVariableArgument(new RDFVariable("?X")),
+                "hello");
+
+            Assert.IsNotNull(builtin);
+            Assert.IsFalse(builtin.IsMathBuiltIn);
+            Assert.IsFalse(builtin.IsComparisonFilterBuiltIn);
+            Assert.IsTrue(builtin.IsStringFilterBuiltIn);
+            Assert.IsNotNull(builtin.IRI);
+            Assert.IsTrue(string.Equals("http://www.w3.org/2003/11/swrlb#endsWith", builtin.IRI));
+            Assert.IsNull(builtin.Literal);
+            Assert.IsNotNull(builtin.LeftArgument);
+            Assert.IsTrue(builtin.LeftArgument is SWRLVariableArgument vlarg
+                            && vlarg.GetVariable().Equals(new RDFVariable("?X")));
+            Assert.IsNotNull(builtin.RightArgument);
+            Assert.IsTrue(builtin.RightArgument is SWRLLiteralArgument rlarg
+                            && rlarg.GetLiteral().Equals(new RDFPlainLiteral("hello")));
+            Assert.IsTrue(string.Equals("swrlb:endsWith(?X,\"hello\")", builtin.ToString()));
+            Assert.ThrowsException<OWLException>(() => SWRLBuiltIn.EndsWith(null, "hello"));
+            Assert.ThrowsException<OWLException>(() => SWRLBuiltIn.EndsWith(new SWRLVariableArgument(new RDFVariable("?X")), null));
+        }
+
+        [TestMethod]
+        public void ShouldSerializeEndsWithBuiltIn()
+        {
+            SWRLBuiltIn builtin = SWRLBuiltIn.EndsWith(
+                new SWRLVariableArgument(new RDFVariable("?X")), "hello");
+
+            Assert.IsTrue(string.Equals("<BuiltInAtom IRI=\"http://www.w3.org/2003/11/swrlb#endsWith\"><Variable IRI=\"urn:swrl:var#X\" /><Literal>hello</Literal></BuiltInAtom>", OWLSerializer.SerializeObject(builtin)));
+        }
+
+        [TestMethod]
+        public void ShouldDeserializeEndsWithBuiltIn()
+        {
+            SWRLBuiltIn builtin = OWLSerializer.DeserializeObject<SWRLBuiltIn>(
+@"<BuiltInAtom IRI=""http://www.w3.org/2003/11/swrlb#endsWith""><Variable IRI=""urn:swrl:var#X"" /><Literal>hello</Literal></BuiltInAtom>");
+
+            Assert.IsNotNull(builtin);
+            Assert.IsFalse(builtin.IsMathBuiltIn);
+            Assert.IsFalse(builtin.IsComparisonFilterBuiltIn);
+            Assert.IsTrue(builtin.IsStringFilterBuiltIn);
+            Assert.IsNotNull(builtin.IRI);
+            Assert.IsTrue(string.Equals("http://www.w3.org/2003/11/swrlb#endsWith", builtin.IRI));
+            Assert.IsNull(builtin.Literal);
+            Assert.IsNotNull(builtin.LeftArgument);
+            Assert.IsTrue(builtin.LeftArgument is SWRLVariableArgument vlarg
+                            && vlarg.GetVariable().Equals(new RDFVariable("?X")));
+            Assert.IsNotNull(builtin.RightArgument);
+            Assert.IsTrue(builtin.RightArgument is SWRLLiteralArgument rlarg
+                            && rlarg.GetLiteral().Equals(new RDFPlainLiteral("hello")));
+            Assert.IsTrue(string.Equals("swrlb:endsWith(?X,\"hello\")", builtin.ToString()));
+            Assert.IsTrue(string.Equals("<BuiltInAtom IRI=\"http://www.w3.org/2003/11/swrlb#endsWith\"><Variable IRI=\"urn:swrl:var#X\" /><Literal>hello</Literal></BuiltInAtom>", OWLSerializer.SerializeObject(builtin)));
+        }
+
+        [TestMethod]
+        public void ShouldEvaluateEndsWithBuiltIn()
+        {
+            DataTable antecedentResults = new DataTable();
+            antecedentResults.Columns.Add("?X");
+            antecedentResults.Rows.Add("FC Internazionale Milano is the best!");
+            antecedentResults.Rows.Add("inter is the best");
+            antecedentResults.Rows.Add("-2^^http://www.w3.org/2001/XMLSchema#int");
+            antecedentResults.Rows.Add(DBNull.Value);
+            antecedentResults.Rows.Add("hello@EN");
+
+            SWRLBuiltIn builtin = SWRLBuiltIn.EndsWith(
+                new SWRLVariableArgument(new RDFVariable("?X")), "best!");
+
+            DataTable builtinResults = builtin.EvaluateOnAntecedent(antecedentResults);
+
+            Assert.IsNotNull(builtinResults);
+            Assert.IsTrue(builtinResults.Columns.Count == 1);
+            Assert.IsTrue(builtinResults.Rows.Count == 1);
+            Assert.IsTrue(string.Equals(builtinResults.Rows[0]["?X"].ToString(), "FC Internazionale Milano is the best!"));
+
+            //Test with unexisting variables
+
+            SWRLBuiltIn builtin2 = SWRLBuiltIn.EndsWith(
+                new SWRLVariableArgument(new RDFVariable("?Z")), "hello"); //unexisting
+            DataTable builtinResults2 = builtin2.EvaluateOnAntecedent(antecedentResults);
+            Assert.IsNotNull(builtinResults2);
+            Assert.IsTrue(builtinResults2.Columns.Count == 1);
+            Assert.IsTrue(builtinResults2.Rows.Count == 5); //SPARQL evaluates true every row if the column is unknown
+        }
+
         #endregion
     }
 }
