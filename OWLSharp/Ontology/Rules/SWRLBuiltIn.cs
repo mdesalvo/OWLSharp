422316c732c491b8f4ba69da22d3e63469fcadde
mdesalvo <floating_point_9@msn.com>
2024-09-14 10:52:23 +0200

    Added support for swrlb:divide

--- OWLSharp/Ontology/Rules/SWRLBuiltIn.cs
+++ OWLSharp/Ontology/Rules/SWRLBuiltIn.cs
@@ -96,6 +96,15 @@ namespace OWLSharp.Ontology.Rules
                     }
                 };
 
+        public static SWRLBuiltIn Divide(SWRLArgument leftArg, params SWRLArgument[] rightArgs)
+            =>  new SWRLBuiltIn()
+                {
+                    IRI = "http://www.w3.org/2003/11/swrlb#divide",
+                    Arguments = Enumerable.Concat(
+                        new List<SWRLArgument>() { leftArg ?? throw new OWLException("Cannot create swrlb:divide builtIn because: left argument is null") },
+                        rightArgs?.ToList() ?? throw new OWLException("Cannot create swrlb:divide builtIn because: right arguments are null")).ToList()
+                };
+
         public static SWRLBuiltIn Floor(SWRLArgument leftArg, SWRLArgument rightArg)
             =>  new SWRLBuiltIn()
                 {
@@ -212,6 +221,9 @@ namespace OWLSharp.Ontology.Rules
                             break;
                         case "http://www.w3.org/2003/11/swrlb#cos":
                             keepRow = SWRLCosBuiltIn.EvaluateOnAntecedent(currentRow, Arguments);
+                            break;
+                        case "http://www.w3.org/2003/11/swrlb#divide":
+                            keepRow = SWRLDivideBuiltIn.EvaluateOnAntecedent(currentRow, Arguments);
                             break;
                         case "http://www.w3.org/2003/11/swrlb#floor":
                             keepRow = SWRLFloorBuiltIn.EvaluateOnAntecedent(currentRow, Arguments);
