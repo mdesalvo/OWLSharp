e12a26869358e401a0ee72b0a1d5f6334ed0e488
Marco De Salvo <marco.desalvo@vodafone.com>
2024-12-17 14:56:57 +0100

    Completed declarer API

---
+++ OWLSharp.Test/Ontology/Rules/Helpers/SWRLHelperTest.cs
@@ -0,0 +1,54 @@
+/*
+   Copyright 2014-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OWLSharp.Ontology;
+using RDFSharp.Model;
+
+namespace OWLSharp.Test.Ontology
+{
+    [TestClass]
+    public class SWRLHelperTest
+    {
+        #region Tests
+		[TestMethod]
+        public void ShouldDeclareRule()
+        {
+            OWLOntology ontology = new OWLOntology();
+            ontology.DeclareRule(new SWRLRule(
+                new RDFPlainLiteral("SWRL1"),
+                new RDFPlainLiteral("This is a test SWRL rule"),
+                new SWRLAntecedent(),
+                new SWRLConsequent()));
+
+            Assert.IsTrue(ontology.Rules.Count == 1);
+            Assert.IsTrue(ontology.CheckHasRule(new SWRLRule(
+                new RDFPlainLiteral("SWRL1"),
+                new RDFPlainLiteral("This is a test SWRL rule"),
+                new SWRLAntecedent(),
+                new SWRLConsequent())));
+            Assert.ThrowsException<OWLException>(() => ontology.DeclareRule(null));
+
+            ontology.DeclareRule(new SWRLRule(
+                new RDFPlainLiteral("SWRL1"),
+                new RDFPlainLiteral("This is a test SWRL rule"),
+                new SWRLAntecedent(),
+                new SWRLConsequent())); //will be discarded, since duplicates are not allowed
+            Assert.IsTrue(ontology.Rules.Count == 1);
+        }
+        #endregion
+    }
+}
---
+++ OWLSharp/Ontology/Rules/Helpers/SWRLHelper.cs
@@ -0,0 +1,39 @@
+﻿/*
+   Copyright 2014-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Linq;
+
+namespace OWLSharp.Ontology
+{
+    public static class SWRLHelper
+    {
+        #region Methods
+        public static bool CheckHasRule(this OWLOntology ontology, SWRLRule rule)
+            => ontology?.Rules.Any(rl => string.Equals(rl.GetXML(), rule?.GetXML())) ?? false;
+
+        public static void DeclareRule(this OWLOntology ontology, SWRLRule rule)
+        {
+            #region Guards
+            if (rule == null)
+                throw new OWLException("Cannot declare SWRL rule because given \"rule\" parameter is null");
+            #endregion
+
+            if (!CheckHasRule(ontology, rule))
+                ontology?.Rules.Add(rule);
+        }
+        #endregion
+    }
+}
