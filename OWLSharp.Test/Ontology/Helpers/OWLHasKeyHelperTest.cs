d9d6fe2f40220c06a675050276480c23ee0c5664
Marco De Salvo <marco.desalvo@vodafone.com>
2024-12-17 11:41:52 +0100

    Continue working on declarer API

---
+++ OWLSharp.Test/Ontology/Helpers/OWLHasKeyHelperTest.cs
@@ -0,0 +1,54 @@
+/*
+   Copyright 2014-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OWLSharp.Ontology;
+using OWLSharp.Ontology.Axioms;
+using OWLSharp.Ontology.Expressions;
+using OWLSharp.Ontology.Helpers;
+using RDFSharp.Model;
+
+namespace OWLSharp.Test.Ontology.Helpers
+{
+    [TestClass]
+    public class OWLHasKeyHelperTest
+    {
+        #region Tests
+		[TestMethod]
+        public void ShouldDeclareHasKey()
+        {
+            OWLOntology ontology = new OWLOntology();
+            ontology.DeclareHasKey(new OWLHasKey(
+                new OWLClass(RDFVocabulary.FOAF.AGENT),
+                [new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)],
+                [new OWLDataProperty(RDFVocabulary.FOAF.AGE)]));
+
+            Assert.IsTrue(ontology.KeyAxioms.Count == 1);
+            Assert.IsTrue(ontology.CheckHasKey(new OWLHasKey(
+                new OWLClass(RDFVocabulary.FOAF.AGENT),
+                [new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)],
+                [new OWLDataProperty(RDFVocabulary.FOAF.AGE)])));
+            Assert.ThrowsException<OWLException>(() => ontology.DeclareHasKey(null));
+
+            ontology.DeclareHasKey(new OWLHasKey(
+                new OWLClass(RDFVocabulary.FOAF.AGENT),
+                [new OWLObjectProperty(RDFVocabulary.FOAF.KNOWS)],
+                [new OWLDataProperty(RDFVocabulary.FOAF.AGE)])); //will be discarded, since duplicates are not allowed
+            Assert.IsTrue(ontology.KeyAxioms.Count == 1);
+        }
+        #endregion
+    }
+}
---
+++ OWLSharp/Ontology/Helpers/OWLHasKeyHelper.cs
@@ -0,0 +1,40 @@
+﻿/*
+   Copyright 2014-2024 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using OWLSharp.Ontology.Axioms;
+using System.Linq;
+
+namespace OWLSharp.Ontology.Helpers
+{
+    public static class OWLHasKeyHelper
+    {
+        #region Methods
+        public static bool CheckHasKey(this OWLOntology ontology, OWLHasKey hasKey)
+            => ontology?.KeyAxioms.Any(ax => string.Equals(ax.GetXML(), hasKey?.GetXML())) ?? false;
+
+        public static void DeclareHasKey(this OWLOntology ontology, OWLHasKey hasKey)
+        {
+            #region Guards
+            if (hasKey == null)
+                throw new OWLException("Cannot declare has key because given \"hasKey\" parameter is null");
+            #endregion
+
+            if (!CheckHasKey(ontology, hasKey))
+                ontology?.KeyAxioms.Add(hasKey);
+        }
+        #endregion
+    }
+}
