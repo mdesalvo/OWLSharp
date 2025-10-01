a5dbbb86402475ecd1bc2901eddd0dceb88512af
Marco De Salvo <marco.desalvo@vodafone.com>
2025-10-01 11:58:33 +0200

    Continue Intellisense work

--- OWLSharp/Validator/OWLIssue.cs
+++ OWLSharp/Validator/OWLIssue.cs
@@ -13,22 +13,41 @@
 
 namespace OWLSharp.Validator
 {
+    /// <summary>
+    /// OWLIssue represents a modeling pitfall or inconsistency discovered by a validator on an ontology
+    /// </summary>
     public sealed class OWLIssue
     {
         #region Properties
+        /// <summary>
+        /// The severity of the issue
+        /// </summary>
         public OWLEnums.OWLIssueSeverity Severity { get; }
+        /// <summary>
+        /// The name of the validator rule which evidenced the issue
+        /// </summary>
         public string RuleName { get; }
+        /// <summary>
+        /// The description of the issue
+        /// </summary>
         public string Description { get; }
+        /// <summary>
+        /// A potential action to be considered for resolving the issue
+        /// </summary>
         public string Suggestion { get; }
         #endregion
 
         #region Ctors
+        /// <summary>
+        /// Builds an issue with the given level of severity and the given characteristics
+        /// </summary>
+        /// <exception cref="OWLException"></exception>
         public OWLIssue(OWLEnums.OWLIssueSeverity severity , string ruleName, string description, string suggestion)
         {
             Severity = severity;
-            RuleName = ruleName?.Trim() ?? throw new OWLException("Cannot emit issue because given \"ruleName\" parameter is null");
-            Description = description?.Trim() ?? throw new OWLException("Cannot emit issue because given \"description\" parameter is null");
-            Suggestion = suggestion?.Trim() ?? throw new OWLException("Cannot emit issue because given \"suggestion\" parameter is null");
+            RuleName = ruleName?.Trim() ?? throw new OWLException($"Cannot emit issue because given '{nameof(ruleName)}' parameter is null");
+            Description = description?.Trim() ?? throw new OWLException($"Cannot emit issue because given '{nameof(description)}' parameter is null");
+            Suggestion = suggestion?.Trim() ?? throw new OWLException($"Cannot emit issue because given '{nameof(suggestion)}' parameter is null");
         }
         #endregion
     }
