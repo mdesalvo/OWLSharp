4f0a27f792b621b3b22b3fd9d015ee45050baf44
Marco De Salvo <marco.desalvo@vodafone.com>
2025-10-03 10:31:41 +0200

    More work on Intellisense

--- OWLSharp/Ontology/Rules/SWRLException.cs
+++ OWLSharp/Ontology/Rules/SWRLException.cs
@@ -18,13 +18,26 @@ using System;
 
 namespace OWLSharp.Ontology
 {
+    /// <summary>
+    /// SWRLException represents an exception thrown during manipulation of SWRL rules
+    /// </summary>
+    [Serializable]
     public sealed class SWRLException : Exception
     {
         #region Ctors
+        /// <summary>
+        /// Builds an empty SWRLException
+        /// </summary>
         public SWRLException() { }
 
+        /// <summary>
+        /// Builds a SWRLException with the given message
+        /// </summary>
         public SWRLException(string message) : base(message) { }
 
+        /// <summary>
+        /// Builds a SWRLException with the given message and inner exception
+        /// </summary>
         public SWRLException(string message, Exception innerException) : base(message, innerException) { }
         #endregion
     }
